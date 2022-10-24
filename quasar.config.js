92ec74edd0b78bc0da1f747455002af4084b9cb3
Arnon <arnonrdp@gmail.com>
2022-10-24 14:43:28 -0300

    Using `/dist` for build

--- quasar.config.js
+++ quasar.config.js
@@ -52,7 +52,7 @@ module.exports = configure(function (ctx) {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
-      distDir: 'public',
+      distDir: 'dist',
       vueRouterMode: 'hash' // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
