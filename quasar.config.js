8e7b3a16d7af094b227f3d73777a8f6a3bd1a2ec
Arnon <arnonrdp@gmail.com>
2022-10-25 15:01:24 -0300

    Defined variables

--- quasar.config.js
+++ quasar.config.js
@@ -84,7 +84,21 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        brand: {
+          primary: '#e54757',
+          secondary: '#5e6776',
+          accent: '#9C27B0',
+
+          dark: '#1d1d1d',
+          'dark-page': '#121212',
+
+          positive: '#21BA45',
+          negative: '#C10015',
+          info: '#31CCEC',
+          warning: '#F2C037'
+        }
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
