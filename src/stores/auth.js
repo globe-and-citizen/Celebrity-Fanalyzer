caa6232edf5849deada5174075654799abbedf06
Arnon <arnonrdp@gmail.com>
2022-10-26 17:47:16 -0300

    Added Pinia stores for authentication

---
+++ src/stores/auth.js
@@ -0,0 +1,41 @@
+import { auth, db } from 'src/firebase.js'
+import router from 'src/router'
+import { useUserStore } from 'src/stores/user'
+import { getAdditionalUserInfo, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import { defineStore } from 'pinia'
+import { LocalStorage } from 'quasar'
+
+export const useAuthStore = defineStore('auth', {
+  actions: {
+    async fetchProfile(user) {
+      const userStore = useUserStore()
+      userStore.fetchUserProfile(user)
+    },
+
+    async googleSignIn() {
+      const provider = new GoogleAuthProvider()
+      await signInWithPopup(auth, provider)
+        .then(async (result) => {
+          const isNewUser = getAdditionalUserInfo(result)?.isNewUser
+          const { email, displayName, photoURL, uid } = result.user
+          if (isNewUser) {
+            await setDoc(doc(db, 'users', uid), { email, displayName, photoURL })
+          }
+          this.fetchProfile(result.user)
+        })
+        .catch((error) => {
+          throw error.code
+        })
+    },
+
+    logout() {
+      const userStore = useUserStore()
+      signOut(auth).then(() => {
+        userStore.$reset()
+        LocalStorage.remove('user')
+        router.push('/login')
+      })
+    }
+  }
+})
---
+++ src/stores/user.js
@@ -0,0 +1,89 @@
+import { auth, db } from 'src/firebase'
+import router from 'src/router'
+import { EmailAuthProvider, reauthenticateWithCredential, updateEmail, updatePassword } from '@firebase/auth'
+import { collection, doc, getDoc, getDocs, runTransaction, updateDoc } from '@firebase/firestore'
+import { defineStore } from 'pinia'
+import { LocalStorage } from 'quasar'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    _user: LocalStorage.getItem('user') || {}
+  }),
+
+  getters: {
+    getUser: (state) => state._user,
+    isAuthenticated: (state) => !!state._user?.uid
+  },
+
+  actions: {
+    async fetchUserProfile(user) {
+      await getDoc(doc(db, 'users', user.uid))
+        .then((document) =>
+          this.$patch({
+            _user: { uid: document.id, ...document.data() }
+          })
+        )
+        .catch((error) => {
+          throw error
+        })
+
+      if (this.getUser) {
+        LocalStorage.set('user', this._user)
+        this.router.push('/')
+      }
+    },
+
+    async checkEmail(email) {
+      const users = await getDocs(collection(db, 'users'))
+
+      const emails = users.docs.map((document) => document.data().email)
+
+      return Boolean(emails.includes(email))
+    },
+
+    async checkUsername(username) {
+      const users = await getDocs(collection(db, 'users'))
+
+      const usernames = users.docs.map((document) => document.data().username.toLowerCase())
+
+      return Boolean(usernames.includes(username.toLowerCase()))
+    },
+
+    async updateAccount(credentials) {
+      const user = auth.currentUser
+      const credential = EmailAuthProvider.credential(this.getUser.email, credentials.password)
+
+      if (!user) return
+
+      await reauthenticateWithCredential(user, credential)
+        .then(async () => {
+          await updateDoc(doc(db, 'users', this.getUser.uid), { email: credentials.email })
+          updateEmail(user, credentials.email)
+            .then(() => {
+              this.$patch({ _user: { email: credentials.email } })
+              LocalStorage.set('user', this._user)
+            })
+            .catch((error) => {
+              throw error.code
+            })
+          updatePassword(user, credentials.newPass)
+        })
+        .catch((error) => {
+          throw error.code
+        })
+    },
+
+    async updateProfile(user) {
+      await runTransaction(db, async (transaction) => {
+        transaction.update(doc(db, 'users', this.getUser.uid), { ...user })
+      })
+        .then(() => {
+          this._user.displayName = user.displayName
+          this._user.username = user.username
+        })
+        .catch((error) => {
+          throw error.code
+        })
+    }
+  }
+})
