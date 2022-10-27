f24084f6d0cec048aa8011be2b792e16e80c6530
Arnon <arnonrdp@gmail.com>
2022-10-27 18:16:26 -0300

    Added search input

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,10 +1,19 @@
 <template>
-  <q-header class="shadow-1">
-    <q-toolbar class="bg-white q-px-lg">
+  <q-header class="bg-white" elevated>
+    <q-toolbar class="q-px-lg">
       <q-toolbar-title>
         <b class="text-secondary">Search Archive</b>
       </q-toolbar-title>
       <q-btn flat icon="notifications" round size="1rem" text-color="secondary" />
+    </q-toolbar>
+    <q-toolbar>
+      <q-toolbar-title>
+        <q-input class="q-pb-lg q-px-lg" dense label="Search" rounded standout v-model="search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </q-toolbar-title>
     </q-toolbar>
   </q-header>
 
