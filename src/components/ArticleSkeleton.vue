b49ac8e183bd7f2c2fe6c63cb235cb32b56a56a8
Arnon <arnonrdp@gmail.com>
2022-11-07 14:52:38 -0300

    Added padding between elements
    - Added radius to image border;
    - Limited width of `q-skeleton` that represents date and time read.

--- src/components/ArticleSkeleton.vue
+++ src/components/ArticleSkeleton.vue
@@ -1,19 +1,17 @@
 <template>
   <article class="q-pt-md relative-position row">
-    <div class="col-8">
-      <div class="flex items-center">
-        <q-skeleton type="circle" size="2rem"/>
-        <q-skeleton type="rect" class="col-grow q-mb-none q-ml-sm"/>
+    <div class="col-8 q-pr-md">
+      <div class="flex items-center q-mb-sm">
+        <q-skeleton type="circle" size="2rem" />
+        <q-skeleton type="rect" class="col-grow q-mb-none q-ml-sm" />
       </div>
       <div>
-        <q-skeleton type="text"/>
-        <q-skeleton type="text"/>
-        <q-skeleton type="text"/>
+        <q-skeleton type="text" />
+        <q-skeleton type="text" />
+        <q-skeleton type="text" width="60%" />
       </div>
     </div>
-    <q-skeleton class="col-4" square/>
-    <q-separator class="full-width q-mt-md"/>
+    <q-skeleton class="col-4" style="border-radius: 24px" />
+    <q-separator class="full-width q-mt-md" />
   </article>
 </template>
-<script setup>
-</script>
