42e6ffbbe160d7e85610650e7f5553d06f631e2a
Arnon <arnonrdp@gmail.com>
2022-11-17 17:51:45 -0300

    Firebase improvements

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getDownloadURL, getStorage, ref } from 'firebase/storage'
+import { getStorage } from 'firebase/storage'
 
 // Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
