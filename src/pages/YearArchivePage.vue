768a58adb41cb31ed6eab2f9d8bece4a51b95b99
Hermann EHO <hermannleboss@gmail.com>
2022-12-04 13:01:25 +0000

    refactor: refactor YearArchivePage using new Item card

--- src/pages/YearArchivePage.vue
+++ src/pages/YearArchivePage.vue
@@ -2,7 +2,7 @@
   <q-header class="bg-white" elevated>
     <q-toolbar class="q-px-lg">
       <q-toolbar-title>
-        <b class="text-secondary">Search Archive</b>
+        <b class="text-secondary">Search In Year Archive</b>
       </q-toolbar-title>
       <q-btn flat icon="notifications" round size="1rem" text-color="secondary" />
     </q-toolbar>
@@ -31,57 +31,26 @@
         unelevated
       />
     </q-scroll-area>
-
     <q-separator class="q-mb-none q-mt-xs" />
     <section v-if="!prompts.length && promptStore.isLoading">
-      <ArticleSkeleton />
-      <ArticleSkeleton />
-      <ArticleSkeleton />
+      <ArticleSkeleton v-for="index in [0, 1, 2, 3]" :key="index" />
     </section>
-
-    <section v-if="promptStore.getPrompts.filter((prompt) => prompt.categories.includes(category)).length">
-      <article
+    <section>
+      <PromptItemCard
         v-for="prompt in prompts"
-        class="q-pt-md relative-position row"
         :key="prompt?.id"
-        v-ripple:primary
-        @click="goToPrompt(prompt.slug)"
+        :item="prompt"
+        v-show="prompt.categories.includes(category)"
+        :link="`/prompt/${prompt.slug}`"
       >
-        <div class="col-8">
-          <div class="flex items-center">
-            <q-avatar size="2rem">
-              <q-img :src="prompt.author.photoURL" />
-            </q-avatar>
-            <p class="q-mb-none q-ml-sm text-body1">
-              {{ prompt.author.displayName.length > 20 ? prompt.author.displayName.substring(0, 20) + '...' : prompt.author.displayName }}
-            </p>
-          </div>
-          <h2 class="q-mb-none text-body1 text-bold">
-            {{ prompt.title.length > 40 ? prompt.title.substring(0, 40) + '...' : prompt.title }}
-          </h2>
-          <p class="q-my-none text-body2 text-secondary">
-            {{
-              new Date(prompt.created.seconds * 1000 + prompt.created.nanoseconds / 1000000).toLocaleDateString('en-US', {
-                month: 'short',
-                day: 'numeric'
-              })
-            }}
-            &nbsp;•&nbsp; 9 min read
-          </p>
-          <div v-if="category === 'Trending'">
-            <q-badge v-for="(item, i) of prompt.categories" class="q-mx-xs" :key="i" rounded>{{ item }}</q-badge>
-          </div>
-        </div>
-        <q-img class="col-4" :ratio="1" :src="prompt.image" spinner-color="primary" spinner-size="3rem" style="border-radius: 24px" />
-        <!-- TODO: Add 'Selected for you' and two more buttons according to mockup -->
-        <q-separator class="full-width q-mt-md" />
-      </article>
+      </PromptItemCard>
     </section>
   </q-page>
 </template>
 
 <script setup>
 import ArticleSkeleton from 'src/components/ArticleSkeleton.vue'
+import PromptItemCard from 'src/components/ItemCard.vue'
 import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import { usePromptStore } from 'src/stores'
@@ -112,8 +81,4 @@ onMounted(async () => {
   await promptStore.fetchPrompts()
   prompts.value = promptStore.getPrompts
 })
-
-function goToPrompt(slug) {
-  router.push(`/prompt/${slug}`)
-}
 </script>
