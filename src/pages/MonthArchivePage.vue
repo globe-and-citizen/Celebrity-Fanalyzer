bf73915fb9ebad4747ae5a8eeddd4383ec1b4476
Hermann EHO <hermannleboss@gmail.com>
2022-12-04 13:01:49 +0000

    refactor: refactor MonthArchivePage.vue using new Item card

--- src/pages/MonthArchivePage.vue
+++ src/pages/MonthArchivePage.vue
@@ -2,7 +2,7 @@
   <q-header class="bg-white" elevated>
     <q-toolbar class="q-px-lg">
       <q-toolbar-title>
-        <b class="text-secondary">Search Archive</b>
+        <b class="text-secondary">Search in Month Entries Archive</b>
       </q-toolbar-title>
       <q-btn flat icon="notifications" round size="1rem" text-color="secondary" />
     </q-toolbar>
@@ -16,17 +16,29 @@
       </q-toolbar-title>
     </q-toolbar>
   </q-header>
-  <q-page>
-    <h2>Month Entries</h2>
+  <q-page class="q-pa-md">
+    <h2 class="q-my-auto text-bold text-h5">{{year}}, {{month}} entries</h2>
+    <q-separator class="q-mb-none q-mt-xs" />
+    <section v-if="!entries.length && entryStore.isLoading">
+      <ArticleSkeleton v-for="index in [0, 1, 2, 3]" :key="index" />
+    </section>
+    <section>
+      <PromptItemCard v-for="entry in entries" :key="entry?.id" :item="entry" :link="`/entry/${entry.slug}`"></PromptItemCard>
+    </section>
   </q-page>
 </template>
 
 <script setup>
+import ArticleSkeleton from 'src/components/ArticleSkeleton.vue'
+import PromptItemCard from 'src/components/ItemCard.vue'
 import { useRouter } from 'vue-router'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import { useEntryStore } from 'src/stores'
 
+const entryStore = useEntryStore()
 const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
+const entries = ref(entryStore.getEntries)
 const router = useRouter()
 const year = ref('')
 const month = ref('')
@@ -35,6 +47,11 @@ if (monthList.includes(router.currentRoute.value.params.month)) {
   month.value = router.currentRoute.value.params.month
 }
 year.value = router.currentRoute.value.params.year
+
+onMounted(async () => {
+  await entryStore.fetchEntries()
+  entries.value = entryStore.getEntries
+})
 </script>
 
 <style scoped></style>
