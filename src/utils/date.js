b7d3e37eae014885b217bb4e5d1a97b49c2ca978
Arnon <arnonrdp@gmail.com>
2022-12-05 18:57:27 -0300

    Created months const to be reused

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,13 +1,20 @@
-function shortMonthDay(timestamp = undefined) {
-  let date
+export const months = [
+  { label: 'January', value: 1 },
+  { label: 'February', value: 2 },
+  { label: 'March', value: 3 },
+  { label: 'April', value: 4 },
+  { label: 'May', value: 5 },
+  { label: 'June', value: 6 },
+  { label: 'July', value: 7 },
+  { label: 'August', value: 8 },
+  { label: 'September', value: 9 },
+  { label: 'October', value: 10 },
+  { label: 'November', value: 11 },
+  { label: 'December', value: 12 }
+]
 
-  if (timestamp) {
-    date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000)
-  } else {
-    date = new Date()
-  }
+export function shortMonthDay(timestamp) {
+  const date = timestamp ? new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000) : new Date()
 
   return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' }) // Nov 2
 }
-
-export { shortMonthDay }
