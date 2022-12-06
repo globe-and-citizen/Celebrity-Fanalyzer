c79b6b81c7ee713529a420eac889ae01b7dfab7e
Arnon <arnonrdp@gmail.com>
2022-12-06 18:25:16 -0300

    Removed unused

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,18 +1,3 @@
-export const monthsOfTheYear = [
-  { label: 'January', value: 1 },
-  { label: 'February', value: 2 },
-  { label: 'March', value: 3 },
-  { label: 'April', value: 4 },
-  { label: 'May', value: 5 },
-  { label: 'June', value: 6 },
-  { label: 'July', value: 7 },
-  { label: 'August', value: 8 },
-  { label: 'September', value: 9 },
-  { label: 'October', value: 10 },
-  { label: 'November', value: 11 },
-  { label: 'December', value: 12 }
-]
-
 export function shortMonthDay(timestamp) {
   const date = timestamp ? new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000) : new Date()
 
