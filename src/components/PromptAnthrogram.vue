359dc990d832f7f8ba90de826d30236e102a74c6
Hermann EHO <hermannleboss@gmail.com>
2022-12-13 17:16:51 +0000

    feat: create BarChart

--- src/components/PromptAnthrogram.vue
+++ src/components/PromptAnthrogram.vue
@@ -6,18 +6,21 @@
           <q-img :src="prompt.author.photoURL" />
         </q-avatar>
         <p class="q-mb-none q-ml-sm text-body1">
-          {{ prompt.author.displayName?.length > 20 ? prompt.author.displayName.substring(0, 20) + '...' : prompt.author.displayName
-          }} </p>
+          {{ prompt.author.displayName?.length > 20 ? prompt.author.displayName.substring(0, 20) + '...' : prompt.author.displayName }}
+        </p>
       </div>
       <h2 class="q-mb-none text-body1 text-bold">
-        {{ prompt.title?.length > 40 ? prompt.title.substring(0, 40) + '...' : prompt.title }} </h2>
+        {{ prompt.title?.length > 40 ? prompt.title.substring(0, 40) + '...' : prompt.title }}
+      </h2>
     </div>
     <q-separator class="full-width q-mt-md" />
-
+    <BarChart />
   </div>
 </template>
 
 <script setup>
+import BarChart from 'src/components/BarChart.vue'
+
 const props = defineProps({
   prompt: {}
 })
