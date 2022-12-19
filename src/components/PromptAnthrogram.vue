68ad2f553b09c24c499844f73232153bc5aaf4a8
Hermann EHO <hermannleboss@gmail.com>
2022-12-19 06:42:14 +0000

    feat: create StatsBarChart
    StatsBarChart working but unable to fetch data from statcounter api

--- src/components/PromptAnthrogram.vue
+++ src/components/PromptAnthrogram.vue
@@ -15,21 +15,31 @@
     </div>
     <p class="q-pt-md text-center">Likes & Dislikes</p>
     <LikeDislikeBarChart :likes="prompt.info.likes.length" :dislikes="prompt.info.dislikes.length" class="q-mt-md" />
+    <StatsBarChart :week1="['162', '131', '7', '124']" :week2="['171', '117', '10', '107']"></StatsBarChart>
   </div>
 </template>
 
 <script setup>
-import { useFetch } from '@vueuse/core'
+import { ref } from 'vue'
 import sha1 from 'js-sha1'
 import LikeDislikeBarChart from 'src/components/LikeDislikeBarChart.vue'
+import StatsBarChart from 'src/components/StatsBarChart.vue'
 
 const props = defineProps({
-  prompt: {}
+  prompt: {
+    type:     Object,
+    required: true
+  }
 })
 const date = Date.now().toString().slice(0, 10)
 const str = `?vn=3&s=summary&f=json&pi=2292634&t=${date}&u=demo_userstatcounter`
 const sha = sha1(str)
-let url = `https://api.statcounter.com/stats/?vn=3&s=summary&f=json&pi=2292634&t=${date}&u=demo_user&sha1=${sha}`
+let refUrl = ref(`https://api.statcounter.com/stats/?vn=3&s=summary&f=json&pi=2292634&t=${date}&u=demo_user&sha1=${sha}`)
+const url = `https://api.statcounter.com/stats/?vn=3&s=summary&f=json&pi=2292634&t=${date}&u=demo_user&sha1=${sha}`
 
-const { isFetching, error, data } = useFetch(url, { mode: 'no-cors' }).get().text()
+fetch(url, { mode: 'no-cors', redirect: 'follow' })
+  .then((data) => console.log('log of response', data))
+  .catch((err) => {
+    console.log('Fetch Error :-S', err)
+  })
 </script>
---
+++ src/components/StatsBarChart.vue
@@ -0,0 +1,55 @@
+<template>
+  <Bar :options="chartOptions" :data="chartData" datasetIdKey="Likes" class="shadow-1 q-pa-md"
+       style="border-radius: 5px" />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name:       'StatsBarChart',
+  components: { Bar },
+  props: {
+    week1: {
+      type: Array,
+      required: true
+    },
+    week2: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    chartData() {
+      return {
+        labels:   ['Page views', 'Unique visits', 'Returning visits', 'First time visits'],
+        datasets: [
+          {
+            data: this.week1,
+            label:           'Week 1',
+            backgroundColor: ['rgb(255, 99, 132)']
+          },
+          {
+            data: this.week2,
+            label:           'Week 2',
+            backgroundColor: ['rgb(54, 162, 235)']
+          }
+        ]
+      }
+    },
+    chartOptions() {
+      return {
+        responsive: true,
+        plugins:    {
+          legend: {
+            display: true
+          }
+        }
+      }
+    }
+  }
+}
+</script>
