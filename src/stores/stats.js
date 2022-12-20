71ed3028f0d0559398116a86ee7e0d9757859f20
Arnon <arnonrdp@gmail.com>
2022-12-20 18:46:45 -0300

    Removed slash

--- src/stores/stats.js
+++ src/stores/stats.js
@@ -18,7 +18,7 @@ export const useStatStore = defineStore('stats', {
       const projectId = '12785650'
       const timestamp = Math.floor(Date.now() / 1000)
       const SHA1 = sha1(`?vn=3&s=summary&pi=${projectId}&t=${timestamp}&u=arnonrdp4L!Ljtn48^+_Q1ML`)
-      const url = `http://api.statcounter.com/stats/?vn=3&s=summary&pi=${projectId}&t=${timestamp}&u=arnonrdp&sha1=${SHA1}`
+      const url = `http://api.statcounter.com/stats?vn=3&s=summary&pi=${projectId}&t=${timestamp}&u=arnonrdp&sha1=${SHA1}`
 
       fetch(url)
         .then((response) => response.json())
