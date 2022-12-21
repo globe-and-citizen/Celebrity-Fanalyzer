776b1da672194dd29868a3c396846e772282a473
Hermann EHO <hermannleboss@gmail.com>
2022-12-21 17:26:47 +0000

    fix: fix api call

--- src/stores/stats.js
+++ src/stores/stats.js
@@ -18,12 +18,13 @@ export const useStatStore = defineStore('stats', {
       const projectId = '12785650'
       const timestamp = Math.floor(Date.now() / 1000)
       const SHA1 = sha1(`?vn=3&s=summary&pi=${projectId}&t=${timestamp}&u=arnonrdp4L!Ljtn48^+_Q1ML`)
-      const url = `http://api.statcounter.com/stats?vn=3&s=summary&pi=${projectId}&t=${timestamp}&u=arnonrdp&sha1=${SHA1}`
+      const url = `https://api.statcounter.com/stats?vn=3&s=summary&pi=${projectId}&t=${timestamp}&u=arnonrdp&sha1=${SHA1}`
 
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
-          console.log('Success:', data)
+          this._stats= data
+          console.info('Success:', data)
         })
         .catch((error) => {
           console.error(error)
