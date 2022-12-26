b3e63d1a988a516214fc0154ab969c09fadcb59b
Arnon <arnonrdp@gmail.com>
2022-12-26 18:32:02 -0300

    Added mock data to Bar Graph

--- src/components/BarGraph.vue
+++ src/components/BarGraph.vue
@@ -27,17 +27,36 @@ const option = ref({
   },
   xAxis: {
     type: 'category',
-    data: props.data.map((d) => d.name)
+    // data: props.data.map((d) => d.name)
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    // TODO: use data from props
+    // https://echarts.apache.org/examples/en/editor.html?c=bar-negative
   },
   yAxis: {
     type: 'value'
   },
+  // series: [
+  //   {
+  //     type: 'bar',
+  //     data: props.data.map((d) => d.value),
+  //     colorBy: 'data',
+  //     color: ['#48982a', '#ea3423', '#f9a61a', '#2e7bb4', '#3ba272', '#fc8452', '#9a60b4', '#ea7ccc']
+  //   }
+  // ]
   series: [
     {
+      name: 'Likes',
       type: 'bar',
-      data: props.data.map((d) => d.value),
-      colorBy: 'data',
-      color: ['#48982a', '#ea3423', '#f9a61a', '#2e7bb4', '#3ba272', '#fc8452', '#9a60b4', '#ea7ccc']
+      stack: 'Total',
+      data: [320, 302, 341, 374, 390, 450, 420],
+      color: '#48982a'
+    },
+    {
+      name: 'Dislikes',
+      type: 'bar',
+      stack: 'Total',
+      data: [-120, -132, -101, -134, -190, -230, -210],
+      color: '#ea3423'
     }
   ]
 })
