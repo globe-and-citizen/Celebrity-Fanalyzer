bf93d4d559b14380d41d06fb42029d63e1d3568f
stravid87 <ravi.student.dev@gmail.com>
2023-01-11 11:52:46 -0500

    test (CelebrityFanazlyer): Add Reference Test File
    This file will not ultimately be saved. It is only meant as a potential guide to accompany the tutorial: https://test-utils.vuejs.org/guide/
    
    Modifications have been made so that it works with Quasar. As work progresses, the file is expected to change and then ultimately be deleted and be replaced by legitimate testing files.

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id" data-test="todo" :class="[todo.completed ? 'completed' : '']">
+      {{ todo.text }}
+      <input type="checkbox" v-model="todo.completed" data-test="todo-checkbox" />
+    </div>
+
+    <form data-test="form" @submit.prevent="createTodo">
+      <input data-test="new-todo" v-model="newTodo" />
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+
+const todos = ref([
+  {
+    id: 1,
+    text: 'Learn Vue.js 3',
+    completed: false
+  }
+])
+
+function createTodo() {
+  console.log('yup clicked')
+  todos.value.push({
+    id: 2,
+    text: newTodo.value,
+    completed: false
+  })
+  console.log(todos.value)
+}
+</script>
