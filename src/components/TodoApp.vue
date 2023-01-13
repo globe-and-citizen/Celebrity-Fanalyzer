cf744a5ff8eeec3b43502c2f069e309f2e1c7793
stravid87 <ravi.student.dev@gmail.com>
2023-01-13 16:03:59 -0500

    test (CF): Experiments with Unit Testing
    More experimentation and progression in learning about Unit Testing.

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div v-for="todo in todos" :key="todo.id" data-test="todo" :class="[todo.completed ? 'completed' : '']">
+  <div id="div1" class="myDiv">
+    <div id="div2" v-for="todo in todos" :key="todo.id" data-test="todo" :class="[todo.completed ? 'completed' : '']">
       {{ todo.text }}
       <input type="checkbox" v-model="todo.completed" data-test="todo-checkbox" />
     </div>
@@ -8,13 +8,29 @@
     <form data-test="form" @submit.prevent="createTodo">
       <input data-test="new-todo" v-model="newTodo" />
     </form>
+
+    <button id="btn1" @click="emitEvent">Click-to-Emit!</button>
+
+    <div id="div3">
+      <div class="toggleDiv" @mouseover="toggleP = !toggleP">Toggle Div</div>
+      <p id="visibilityP" v-if="toggleP">I show when the toggle switch is true. Mouseover "toggleDiv" to toggle me on/off</p>
+    </div>
+
+    <todo-child :title="title" :data="dataArray"></todo-child>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import TodoChild from './TodoChild.vue'
+
+const title = ref('My Cool Title')
+const dataArray = ref(['oNe', 'tWo', 'tHree'])
+
+const toggleP = ref(true)
 
 const newTodo = ref('')
+const emit = defineEmits(['e1', 'e2', 'e3'])
 
 const todos = ref([
   {
@@ -24,8 +40,11 @@ const todos = ref([
   }
 ])
 
+function emitEvent() {
+  emit('e1')
+}
+
 function createTodo() {
-  console.log('yup clicked')
   todos.value.push({
     id: 2,
     text: newTodo.value,
--- src/components/__tests__/TodoApp.spec.js
+++ src/components/__tests__/TodoApp.spec.js
@@ -2,10 +2,13 @@ import { installQuasar } from '@quasar/quasar-app-extension-testing-unit-vitest'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it, test } from 'vitest'
 import TodoApp from '../TodoApp.vue'
+import TodoChild from '../TodoChild.vue'
 
 installQuasar()
 
-describe('Todo', () => {
+describe('The TodoApp.vue component', () => {
+  const globalWrapper = mount(TodoApp) // A future optimization may be to use a global variable to not repeatedly mount the component under test.
+
   it('Renders / mounts TodoApp.vue', () => {
     const wrapper = mount(TodoApp)
     expect(wrapper).toBeTruthy()
@@ -33,5 +36,67 @@ describe('Todo', () => {
     await wrapper.get('[data-test="todo-checkbox"]').setValue(true)
 
     expect(wrapper.get('[data-test="todo"]').classes()).toContain('completed')
+  })
+
+  it('Emits event "e1" when btn is clicked', () => {
+    const wrapper = mount(TodoApp)
+    wrapper.get('#btn1').trigger('click')
+    expect(wrapper.emitted()).toBeTruthy()
+  })
+
+  it('Mouseover event(s) on div with class "toggleDiv" makes the child paragraph render conditionally', async () => {
+    // GOTCHA: events are asynchronous and must be awaited.
+    // Note: <p> element begins as visible.
+    const wrapper = mount(TodoApp)
+    expect(wrapper.find('#visibilityP').exists()).toBe(true)
+    await wrapper.get('.toggleDiv').trigger('mouseover')
+    expect(wrapper.find('#visibilityP').exists()).toBe(false)
+    await wrapper.get('.toggleDiv').trigger('mouseover')
+    expect(wrapper.get('#visibilityP').exists()).toBeTruthy()
+    await wrapper.get('.toggleDiv').trigger('mouseover')
+    expect(wrapper.find('#visibilityP').exists()).toBe(false)
+  })
+
+  it('Renders "TodoChild" correctly with hardcoded props', () => {
+    // NOTE: Note the use of the global component
+    const todoChildVueWrapper = globalWrapper.getComponent({ name: 'todo-child' })
+    //console.log('===>', todoChildDOMWrapper.get('#div1'))
+    expect(todoChildVueWrapper.get('.heading1').text()).toBe('My Cool Title')
+
+    expect(todoChildVueWrapper.findAll('p')[0].text()).toBe('Value at index 0 is oNe')
+  })
+
+  it('Renders "TodoChild" correctly with dynamic props', async () => {
+    // GOTCHA: Notice the use of async/await. This is becuase when you set props, you must await the rendering.
+
+    let wrapper = mount(TodoChild, {
+      props: {
+        title: 'Title One',
+        data: ['fOUr', 'fIve', 'sIx']
+      }
+    })
+
+    expect(wrapper.get('.heading1').text()).toBe('Title One')
+    expect(wrapper.findAll('p')[0].text()).toBe('Value at index 0 is fOUr')
+    expect(wrapper.findAll('p')[1].text()).toBe('Value at index 1 is fIve')
+    expect(wrapper.findAll('p')[2].text()).toBe('Value at index 2 is sIx')
+
+    // GOTCHA: The component, TodoChild has recieved the updated props but still needs to re-render
+
+    await wrapper.unmount()
+
+    wrapper = mount(TodoChild)
+
+    await wrapper.setProps({
+      title: 'Inserted Title',
+      data: ['sEven', 'eIght', 'nIne']
+    })
+
+    console.log('====> ', wrapper.vm.title)
+    console.log('====> ', wrapper.vm.data)
+
+    console.log('wrapper.props() updated successfully but the component, no: ', wrapper.props())
+
+    expect(wrapper.get('.heading1').text()).toBe('Inserted Title')
   })
 })
