e23e0bf2b9e22408d10f099b5cbb8479249895e1
Arnon <arnonrdp@gmail.com>
2023-01-31 15:04:22 -0300

    Prettier fixes

--- src/components/TheComments.vue
+++ src/components/TheComments.vue
@@ -8,9 +8,7 @@
             <q-img :src="comment.author.photoURL" />
           </q-avatar>
           <p class="column q-mb-none q-ml-sm">
-            <span class="text-body2">{{
-              comment.author.displayName || "Anonymous"
-            }}</span>
+            <span class="text-body2">{{ comment.author.displayName || 'Anonymous' }}</span>
             <span class="text-body2 text-secondary">
               {{ shortMonthDayTime(comment.created) }}
             </span>
@@ -38,11 +36,7 @@
             </q-list>
           </q-btn-dropdown>
         </div>
-        <q-form
-          v-if="isEditing && comment.id === inputEdit"
-          greedy
-          @submit.prevent="editComment(comment.id, comment.text)"
-        >
+        <q-form v-if="isEditing && comment.id === inputEdit" greedy @submit.prevent="editComment(comment.id, comment.text)">
           <q-input
             class="q-px-sm"
             autogrow
@@ -55,60 +49,34 @@
             standout="bg-secondary text-white"
             v-model="comment.text"
           >
-            <q-btn
-              class="cursor-pointer"
-              color="grey-6"
-              flat
-              icon="send"
-              round
-              type="submit"
-            />
+            <q-btn class="cursor-pointer" color="grey-6" flat icon="send" round type="submit" />
           </q-input>
         </q-form>
         <div v-else class="q-my-sm text-body2">
           {{ comment.text }}
         </div>
-        <q-expansion-item
-          group="somegroup"
-          @click="replyInput(comment.id)"
-          icon="chat_bubble_outline"
-        >
+        <q-expansion-item group="somegroup" @click="replyInput(comment.id)" icon="chat_bubble_outline">
           <q-card>
             <q-card-section>
               <q-slide-transition>
-                <q-spinner v-if="isLoading"
-                    color="primary"
-                    size="3em"
-                  />
+                <q-spinner v-if="isLoading" color="primary" size="3em" />
                 <q-form v-else greedy @submit.prevent="addReply(comment.id)">
                   <!-- Started Child Comment section -->
-                  <div
-                    v-for="childComment of childComments"
-                    class="q-mb-md q-ml-xl"
-                    :key="childComment.id"
-                  >
+                  <div v-for="childComment of childComments" class="q-mb-md q-ml-xl" :key="childComment.id">
                     <div class="flex items-center">
-                      <q-icon
-                        v-if="childComment.isAnonymous"
-                        name="person"
-                        size="1.5rem"
-                      />
+                      <q-icon v-if="childComment.isAnonymous" name="person" size="1.5rem" />
                       <q-avatar v-else size="1.5rem">
                         <q-img :src="childComment.author.photoURL" />
                       </q-avatar>
                       <p class="row q-mb-none q-ml-sm">
-                        <span class="text-body2 q-mr-sm">{{
-                          childComment.author.displayName || "Anonymous"
-                        }}</span>
+                        <span class="text-body2 q-mr-sm">{{ childComment.author.displayName || 'Anonymous' }}</span>
                         <span class="text-caption text-secondary">
                           {{ shortMonthDayTime(childComment.created) }}
                         </span>
                       </p>
                       <q-space />
                       <q-btn-dropdown
-                        v-if="
-                          (childComment.author?.uid || childComment.author) === userId
-                        "
+                        v-if="(childComment.author?.uid || childComment.author) === userId"
                         color="secondary"
                         dense
                         dropdown-icon="more_vert"
@@ -116,20 +84,12 @@
                         rounded
                       >
                         <q-list>
-                          <q-item
-                            clickable
-                            v-close-popup
-                            @click="editInput(childComment.id)"
-                          >
+                          <q-item clickable v-close-popup @click="editInput(childComment.id)">
                             <q-item-section>
                               <q-item-label>Edit</q-item-label>
                             </q-item-section>
                           </q-item>
-                          <q-item
-                            clickable
-                            v-close-popup
-                            @click="deleteComment(comment.id, childComment.id)"
-                          >
+                          <q-item clickable v-close-popup @click="deleteComment(comment.id, childComment.id)">
                             <q-item-section>
                               <q-item-label>Delete</q-item-label>
                             </q-item-section>
@@ -150,20 +110,11 @@
                         lazy-rules
                         required
                         rounded
-                        :rules="[
-                          (val) => val.length > 1 || 'Please type at least 2 characters',
-                        ]"
+                        :rules="[(val) => val.length > 1 || 'Please type at least 2 characters']"
                         standout="bg-secondary text-white"
                         v-model="childComment.text"
                       >
-                        <q-btn
-                          class="cursor-pointer"
-                          color="grey-6"
-                          flat
-                          icon="send"
-                          round
-                          type="submit"
-                        />
+                        <q-btn class="cursor-pointer" color="grey-6" flat icon="send" round type="submit" />
                       </q-input>
                     </q-form>
                     <div v-else class="q-my-sm text-body2">
@@ -176,24 +127,15 @@
                     class="q-px-sm"
                     autogrow
                     dense
-                    label="Reply"   
+                    label="Reply"
                     lazy-rules
                     :name="comment.id"
                     rounded
-                    :rules="[
-                      (val) => val.length > 1 || 'Please type at least 2 characters',
-                    ]"
+                    :rules="[(val) => val.length > 1 || 'Please type at least 2 characters']"
                     standout="bg-secondary text-white"
                     v-model="reply.text"
                   >
-                    <q-btn
-                      class="cursor-pointer"
-                      color="grey-6"
-                      flat
-                      icon="send"
-                      round
-                      type="submit"
-                    />
+                    <q-btn class="cursor-pointer" color="grey-6" flat icon="send" round type="submit" />
                   </q-input>
                 </q-form>
               </q-slide-transition>
@@ -223,92 +165,76 @@
 </template>
 
 <script setup>
-import { useQuasar, LocalStorage } from "quasar";
-import { useCommentStore, useUserStore } from "src/stores";
-import { shortMonthDayTime } from "src/utils/date";
-import { onMounted, reactive, ref } from "vue";
-import { useRouter } from "vue-router";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  runTransaction,
-  setDoc,
-  Timestamp,
-  where,
-} from "firebase/firestore";
-import { db } from "src/firebase";
+import { useQuasar } from 'quasar'
+import { useCommentStore, useUserStore } from 'src/stores'
+import { shortMonthDayTime } from 'src/utils/date'
+import { onMounted, reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
-const router = useRouter();
+const router = useRouter()
 
 const props = defineProps({
   comments: { type: Array, required: true },
-  entry: { type: Object, required: true },
-});
+  entry: { type: Object, required: true }
+})
 
-const $q = useQuasar();
-const commentStore = useCommentStore();
-const userStore = useUserStore();
+const $q = useQuasar()
+const commentStore = useCommentStore()
+const userStore = useUserStore()
 
-const inputEdit = ref("");
-const isEditing = ref(false);
-const isReplying = ref(true);
-const myComment = reactive({});
-const reply = reactive({});
-const userId = ref("");
-const isLoading = ref("");
-const childComments = ref([]);
+const inputEdit = ref('')
+const isEditing = ref(false)
+const myComment = reactive({})
+const reply = reactive({})
+const userId = ref('')
+const isLoading = ref('')
+const childComments = ref([])
 
 onMounted(async () => {
-  await userStore.fetchUserIp();
-  userId.value = userStore.getUserRef?.id || userStore.getUserIpHash;
-
-  // TODO: After receiving all comments, we need to organized the replies. Use parentId to know which reply belongs to which comment.
-});
+  await userStore.fetchUserIp()
+  userId.value = userStore.getUserRef?.id || userStore.getUserIpHash
+})
 
 async function addComment() {
   await commentStore
     .addComment(myComment, props.entry)
     .then(() => {
-      myComment.text = "";
-      window.scrollTo(0, document.body.scrollHeight);
-      $q.notify({ message: "Comment successfully submitted" });
+      myComment.text = ''
+      window.scrollTo(0, document.body.scrollHeight)
+      $q.notify({ message: 'Comment successfully submitted' })
     })
-    .catch(() => $q.notify({ message: "Comment submission failed!" }));
+    .catch(() => $q.notify({ message: 'Comment submission failed!' }))
 }
 
 function editInput(commentId) {
-  isEditing.value = !isEditing.value;
-  inputEdit.value = commentId;
+  isEditing.value = !isEditing.value
+  inputEdit.value = commentId
 }
 
 async function editComment(commentId, editedComment) {
   await commentStore
     .editComment(props.entry.id, commentId, editedComment, userId.value)
-    .then(() => $q.notify({ message: "Comment successfully edited!" }))
-    .catch(() => $q.notify({ message: "Failed to edit comment" }))
-    .finally(() => (isEditing.value = false));
+    .then(() => $q.notify({ message: 'Comment successfully edited!' }))
+    .catch(() => $q.notify({ message: 'Failed to edit comment' }))
+    .finally(() => (isEditing.value = false))
 }
 
 async function deleteComment(commentParentId, commentId) {
   await commentStore
     .deleteComment(props.entry.id, commentId, userId.value)
-    .then(() => $q.notify({ message: "Comment successfully deleted" }))
-    .catch(() => $q.notify({ message: "Failed to delete comment" }));
+    .then(() => $q.notify({ message: 'Comment successfully deleted' }))
+    .catch(() => $q.notify({ message: 'Failed to delete comment' }))
 
-  await commentStore.fetchCommentsByparentId(router.currentRoute.value.href, commentParentId);
-  childComments.value = commentStore.getChildComments;
+  await commentStore.fetchCommentsByparentId(router.currentRoute.value.href, commentParentId)
+  childComments.value = commentStore.getChildComments
 }
 
 async function replyInput(parentId) {
-  reply.parentId = parentId;
+  reply.parentId = parentId
 
   isLoading.value = true
-  await commentStore.fetchCommentsByparentId(router.currentRoute.value.href, parentId);
-  childComments.value = commentStore.getChildComments;
+  await commentStore.fetchCommentsByparentId(router.currentRoute.value.href, parentId)
+  childComments.value = commentStore.getChildComments
   isLoading.value = false
 }
 
@@ -316,17 +242,17 @@ async function addReply(commentId) {
   await commentStore
     .addReply(props.entry.id, commentId, reply)
     .then(() => {
-      reply.text = "";
-      $q.notify({ message: "Reply successfully submitted" });
+      reply.text = ''
+      $q.notify({ message: 'Reply successfully submitted' })
     })
-    .catch((err) => $q.notify({ message: "Reply submission failed!" + err }));
-  window.scrollTo(0, document.body.scrollHeight);
+    .catch((err) => $q.notify({ message: 'Reply submission failed!' + err }))
+  window.scrollTo(0, document.body.scrollHeight)
 
-  await commentStore.fetchCommentsByparentId(router.currentRoute.value.href, commentId);
-  childComments.value = commentStore.getChildComments;
+  await commentStore.fetchCommentsByparentId(router.currentRoute.value.href, commentId)
+  childComments.value = commentStore.getChildComments
 }
-//Reply submission failed!FirebaseError: [code=invalid-argument]: Function setDoc() called with invalid data. Unsupported field value: a custom SubmitEvent object (found in field parentId in document entries/2022-08T1674307890920/comments/1674593638715-r3C28i2x4RUuqn2jrt69A5K6RcC3)
 </script>
+
 <style>
 .q-item__section--main ~ .q-item__section--side {
   display: none !important;
