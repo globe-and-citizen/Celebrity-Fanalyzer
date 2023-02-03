a63ff89fb37c16fd34fed5c8c7cdf5a6675ad6db
Hermann EHO <hermannleboss@gmail.com>
2023-02-03 12:05:18 +0000

    test: testing calendarWeek function in date.js

--- src/utils/___test___/date.spec.js
+++ src/utils/___test___/date.spec.js
@@ -1,5 +1,5 @@
 import {Timestamp} from "firebase/firestore";
-import {calendarDay, startEndDay} from "src/utils/date";
+import {calendarDay, calendarWeek, startEndDay} from "src/utils/date";
 import { describe, expect, it } from 'vitest'
 describe("utils/dates.js",  () =>{
   describe('startEndDay', ()=>{
@@ -24,6 +24,27 @@ describe("utils/dates.js",  () =>{
 
      const calendar=  calendarDay(startDate,endDate)
       expect(calendar.length).toBe(3)
+    })
+  })
+
+
+  describe('calendarWeek', ()=>{
+    it("should return Calendar of 3 Weeks" , ()=>{
+      // StartDate = GMT: Saturday 3 December 2022 10:52:03
+      const startDate = new Timestamp(1670064723,0)
+      // endDate = GMT: Tuesday 3 January 2023 22:30:37
+      const endDate = new Timestamp(1672785037,0)
+
+      // calendar should be
+      // Saturday 3 December 2022 00:00:00 : 1670025600
+      // Saturday 10 December 2022 00:00:00 : 1670630400
+      // Saturday 17 December 2022 00:00:00 : 1671235200
+      // Saturday 24 December 2022 00:00:00 : 1671840000
+      // Saturday 31 December 2022 00:00:00 : 1672444800
+      // Wednesday 4 January 2022 00:00:00 : 1672790400
+
+      const calendar=  calendarWeek(startDate,endDate)
+      expect(calendar.length).toBe(6)
     })
   })
 })
