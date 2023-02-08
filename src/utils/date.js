4b16642fb6c31fd3ae5f355c880e11fb89546557
Hermann EHO <hermannleboss@gmail.com>
2023-02-08 11:52:15 +0000

    chore: setup dates const in date utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,46 +1,49 @@
-import { Timestamp } from 'firebase/firestore'
+import {Timestamp} from 'firebase/firestore'
+
+const ONE_DAY_IN_SECONDS = 86400
+const ONE_WEEK_IN_SECONDS = 604800
 
 export function monthYear(date) {
   const [year, month] = date.split('-')
-  const monthName = new Date(year, month - 1).toLocaleDateString('en-US', { month: 'short' })
+  const monthName = new Date(year, month - 1).toLocaleDateString('en-US', {month: 'short'})
   return `${monthName} ${year}` // Dec 2022
 }
 
 export function monthDay(date) {
   const seconds = date.split('T')[1]
   const dateObj = new Date(seconds * 1000)
-  return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric' }) // Dec 10
+  return dateObj.toLocaleDateString('en-US', {month: 'short', day: 'numeric'}) // Dec 10
 }
 
 export function shortMonthDayTime(timestamp) {
   const date = timestamp ? new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000) : new Date()
 
   return date
-    .toLocaleDateString('en-US', { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })
+    .toLocaleDateString('en-US', {month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false})
     .split(', ')
     .join(' - ') // Dec 10 - 14:18
 }
 
 export function currentYearMonth() {
-  return new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit' }).split('/').reverse().join('-') // 2022-11
+  return new Date().toLocaleDateString('en-US', {year: 'numeric', month: '2-digit'}).split('/').reverse().join('-') // 2022-11
 }
 
 export function previousYearMonth() {
   const date = new Date()
   date.setMonth(date.getMonth() - 1)
 
-  return date.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit' }).split('/').reverse().join('-') // 2022-10
+  return date.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit'}).split('/').reverse().join('-') // 2022-10
 }
 
 export function startEndDay(timeStamp) {
   return {
-    start: new Timestamp(timeStamp.seconds - (timeStamp.seconds % 86400), 0),
-    end: new Timestamp(timeStamp.seconds - (timeStamp.seconds % 86400) + 86400, 0)
+    start: new Timestamp(timeStamp.seconds - (timeStamp.seconds % ONE_DAY_IN_SECONDS), 0),
+    end: new Timestamp(timeStamp.seconds - (timeStamp.seconds % ONE_DAY_IN_SECONDS) + ONE_DAY_IN_SECONDS, 0)
   }
 }
 
 export function nextWeekDate(timeStamp) {
-  return new Timestamp(timeStamp.seconds - (timeStamp.seconds % 86400) + 86400 * 7, 0)
+  return new Timestamp(timeStamp.seconds - (timeStamp.seconds % ONE_DAY_IN_SECONDS) + ONE_WEEK_IN_SECONDS, 0)
 }
 
 /**
@@ -55,7 +58,7 @@ export function calendarDay(startDate, endDate) {
   let calendar = []
   for (
     let currentDate = startEndDay(startDate).start;
-    currentDate.seconds < endDate.seconds + 86400;
+    currentDate.seconds < endDate.seconds + ONE_DAY_IN_SECONDS;
     currentDate = startEndDay(currentDate).end
   ) {
     calendar.push(currentDate)
@@ -81,10 +84,11 @@ export function calendarWeek(startDate, endDate) {
   calendar.push(startEndDay(endDate).end)
   return calendar
 }
+
 export const getStats = (reacts, startAt) => {
   let endAt = Timestamp.now()
-  if (endAt - startAt > 2678400) {
-    endAt = new Timestamp(startAt.seconds + 2678400, 0)
+  if (endAt - startAt > ONE_DAY_IN_SECONDS * 31) {
+    endAt = new Timestamp(startAt.seconds + ONE_DAY_IN_SECONDS * 31, 0)
   }
 
   const _calendarDay = calendarDay(startAt, endAt)
@@ -101,9 +105,9 @@ export const getStats = (reacts, startAt) => {
       let dislikesCount = reacts._dislikes.filter((element) => {
         return element.createdAt >= _calendarWeek[index] && element.createdAt < _calendarWeek[index + 1]
       }).length
-      return { date: item, likes: likesCount, dislikes: dislikesCount }
+      return {date: item, likes: likesCount, dislikes: dislikesCount}
     } else {
-      return { date: item, likes: 0, dislikes: 0 }
+      return {date: item, likes: 0, dislikes: 0}
     }
   })
   const dayStats = _calendarDay.map((item, index) => {
@@ -117,11 +121,11 @@ export const getStats = (reacts, startAt) => {
       let dislikesCount = reacts._dislikes.filter((element) => {
         return element.createdAt >= _calendarDay[index] && element.createdAt < _calendarDay[index + 1]
       }).length
-      return { date: item, likes: likesCount, dislikes: dislikesCount }
+      return {date: item, likes: likesCount, dislikes: dislikesCount}
     } else {
-      return { date: item, likes: 0, dislikes: 0 }
+      return {date: item, likes: 0, dislikes: 0}
     }
   })
 
-  return { weekStats, dayStats }
+  return {weekStats, dayStats}
 }
