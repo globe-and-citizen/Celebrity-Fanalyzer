ff119639f5d642680e8ac2a41232776ca5141cf9
Arnon <arnonrdp@gmail.com>
2023-02-09 18:42:59 -0300

    Google Sign and Logout moved to UserStore

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,8 +1,9 @@
-import { doc, getDoc, runTransaction } from '@firebase/firestore'
+import { doc, getDoc, runTransaction, setDoc } from '@firebase/firestore'
+import { getAdditionalUserInfo, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import { defineStore } from 'pinia'
 import { LocalStorage } from 'quasar'
 import sha1 from 'sha1'
-import { db } from 'src/firebase'
+import { auth, db } from 'src/firebase'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
@@ -23,18 +24,6 @@ export const useUserStore = defineStore('user', {
     isLoading: (state) => state._isLoading
   },
   actions: {
-    async fetchUserProfile(user) {
-      this._isLoading = true
-      await getDoc(doc(db, 'users', user.uid))
-        .then((document) => this.$patch({ _user: { uid: document.id, ...document.data() } }))
-        .finally(() => (this._isLoading = false))
-
-      if (this.getUser) {
-        LocalStorage.set('user', this._user)
-        this.router.go(0)
-      }
-    },
-
     /**
      * Fetch the user ip from Cloudflare
      * @SaveState <string> IPV6
@@ -52,6 +41,26 @@ export const useUserStore = defineStore('user', {
         })
     },
 
+    async googleSignIn() {
+      const provider = new GoogleAuthProvider()
+
+      this._isLoading = true
+      await signInWithPopup(auth, provider)
+        .then(async (result) => {
+          const isNewUser = getAdditionalUserInfo(result)?.isNewUser
+          const { email, displayName, photoURL, uid } = result.user
+
+          if (isNewUser) {
+            await setDoc(doc(db, 'users', uid), { email, displayName, photoURL })
+          }
+
+          await getDoc(doc(db, 'users', result.user.uid)).then((document) => {
+            this.$patch({ _user: { uid: document.id, ...document.data() } })
+          })
+        })
+        .finally(() => (this._isLoading = false))
+    },
+
     async updateProfile(user) {
       this._isLoading = true
       await runTransaction(db, async (transaction) => {
@@ -59,6 +68,15 @@ export const useUserStore = defineStore('user', {
       })
         .then(() => this.$patch({ _user: { ...this.getUser, ...user } }))
         .finally(() => (this._isLoading = false))
+    },
+
+    logout() {
+      const userStore = useUserStore()
+      signOut(auth).then(() => {
+        userStore.$reset()
+        LocalStorage.remove('user')
+        this.router.go(0)
+      })
     }
   }
 })
