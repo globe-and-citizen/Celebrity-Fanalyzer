6dfe033fa4956d96539ec4df26c37bf90757785e
Hermann EHO <hermannleboss@gmail.com>
2023-02-10 14:50:48 +0000

    build: config jsdom for vitest

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   test: {
+    environment: 'jsdom',
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'json', 'html'],
