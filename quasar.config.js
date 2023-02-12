9d1e4d683683a7970306494bfd134f14633e4525
Hermann EHO <hermannleboss@gmail.com>
2023-02-12 11:09:49 +0000

    build: config of sentry to track errors

--- quasar.config.js
+++ quasar.config.js
@@ -27,7 +27,9 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
-    boot: [],
+    boot: [
+      '/sentry.js'
+    ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.css'],
@@ -47,6 +49,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      sourceMap: true,
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
