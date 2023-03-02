71ddf4efc564c70e8219ccb0a983ab87912762ef
Arnon <arnonrdp@gmail.com>
2023-03-02 17:22:44 -0300

    Remove subjects group from Entries Fixes #140

--- src/components/TheEntries.vue
+++ src/components/TheEntries.vue
@@ -1,24 +1,9 @@
 <template>
   <section class="q-pa-md">
-    <q-scroll-area :thumb-style="{ display: 'none' }" style="height: 3.8rem">
-      <q-btn-toggle
-        class="q-my-sm"
-        color="white"
-        no-caps
-        no-wrap
-        :options="subjects"
-        rounded
-        unelevated
-        text-color="secondary"
-        v-model="subject"
-      />
-    </q-scroll-area>
     <h2 class="q-my-auto text-bold text-h5">Entries</h2>
     <q-separator />
     <ItemCard v-for="entry in entries" :item="entry" :key="entry?.id" :link="entry.slug" />
-    <div v-if="entryStore.isLoading" class="q-my-xl text-center">
-      <q-spinner color="primary" size="3em" />
-    </div>
+    <q-spinner v-if="entryStore.isLoading" color="primary" size="3em" class="block q-mx-auto q-my-xl" />
     <h6 v-else-if="!entries?.length" class="text-center">NO ENTRIES</h6>
   </section>
 </template>
@@ -26,19 +11,8 @@
 <script setup>
 import ItemCard from 'src/components/ItemCard.vue'
 import { useEntryStore } from 'src/stores'
-import { ref } from 'vue'
 
 const props = defineProps(['entries'])
 
 const entryStore = useEntryStore()
-
-const subject = ref('Multinational Support')
-const subjects = [
-  { label: 'Multinational Support', value: 'Multinational Support' },
-  { label: 'Bipartisanship', value: 'Bipartisanship' },
-  { label: 'Likes', value: 'Likes' },
-  { label: 'Dislikes', value: 'Dislikes' },
-  { label: 'Comments', value: 'Comments' },
-  { label: 'Shares', value: 'Shares' }
-]
 </script>
