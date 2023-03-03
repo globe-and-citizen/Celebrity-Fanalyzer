eec6069458aa290b671eeb67932bc7afc97ae212
Hermann EHO <hermannleboss@gmail.com>
2023-03-03 09:48:20 +0000

    build: install cypress using npx quasar ext add @quasar/testing-e2e-cypress@betaeeeeeee

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const registerCodeCoverageTasks = require('@cypress/code-coverage/task');
+const { injectQuasarDevServerConfig } = require('@quasar/quasar-app-extension-testing-e2e-cypress/cct-dev-server');
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  fixturesFolder: 'test/cypress/fixtures',
+  screenshotsFolder: 'test/cypress/screenshots',
+  videosFolder: 'test/cypress/videos',
+  video: true,
+  e2e: {
+    setupNodeEvents(on, config) {
+      registerCodeCoverageTasks(on, config);
+      return config;
+    },
+    baseUrl: 'http://localhost:9000/',
+    supportFile: 'test/cypress/support/e2e.js',
+    specPattern: 'test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  component: {
+    setupNodeEvents(on, config) {
+      registerCodeCoverageTasks(on, config);
+      return config;
+    },
+    supportFile: 'test/cypress/support/component.js',
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+    indexHtmlFile: 'test/cypress/support/component-index.html',
+    devServer: injectQuasarDevServerConfig(),
+  },
+});
