9cb6f08169ae21f418446b5555c2f98fb59f6c0f
Hermann EHO <hermannleboss@gmail.com>
2023-03-07 14:23:24 +0000

    test: Fix not find content fo user navigationt est

--- test/cypress/e2e/user-navigation.cy.js
+++ test/cypress/e2e/user-navigation.cy.js
@@ -1,11 +1,6 @@
 describe('User Navigation', () => {
   it.only('Should display properly and navigate between pages', () => {
     cy.visit('/')
-    cy.getByData('month-link').click()
-    cy.location("pathname").should(
-      "eq",
-      "/month"
-    )
 
     cy.getByData('main-menu').find("a").eq(0).click()
     cy.location("pathname").should(
@@ -20,17 +15,17 @@ describe('User Navigation', () => {
     )
     // cy.get("h2").contains("Entries")
 
-    // cy.getByData('main-menu').find("a").eq(1).click()
-    // cy.location("pathname").should(
-    //   "eq",
-    //   "/search"
-    // )
-    //
-    // cy.getByData('main-menu').find("a").eq(3).click()
-    // cy.location("pathname").should(
-    //   "eq",
-    //   "/profile"
-    // )
+    cy.getByData('main-menu').find("a").eq(1).click()
+    cy.location("pathname").should(
+      "eq",
+      "/search"
+    )
+
+    cy.getByData('main-menu').find("a").eq(3).click()
+    cy.location("pathname").should(
+      "eq",
+      "/profile"
+    )
     // cy.get("h1").contains("You are not logged in.")
   })
 })
