e1eb0d47200ce9b02ee265ceae2896b6bd739063
Hermann EHO <hermannleboss@gmail.com>
2023-03-07 14:07:56 +0000

    build(fix): fix user journey

--- test/cypress/e2e/user-journey.cy.js
+++ test/cypress/e2e/user-journey.cy.js
@@ -2,11 +2,5 @@ describe('User Journey', () => {
   it('Should display properly and navigate to month page', () => {
     cy.visit('/')
     cy.get('h2').contains('Welcome to Celebrity Fanalyzer!')
-    cy.getByData('month-link').click()
-
-    cy.location("pathname").should(
-      "eq",
-      "/month"
-    )
   })
 })
