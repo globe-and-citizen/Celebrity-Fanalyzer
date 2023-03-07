caf773e33a0f81b00d0a187c4c89cd787faba5c1
Arnon <arnonrdp@gmail.com>
2023-03-07 15:53:26 -0300

    Added function to fetch errors collection

--- src/stores/errors.js
+++ src/stores/errors.js
@@ -1,11 +1,31 @@
-import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import { addDoc, collection, getDocs, Timestamp } from 'firebase/firestore'
 import { defineStore } from 'pinia'
 import { Notify } from 'quasar'
 import { db } from 'src/firebase'
 import { useUserStore } from 'src/stores'
 
 export const useErrorStore = defineStore('errors', {
+  state: () => ({
+    _errors: [],
+    _isLoading: false
+  }),
+
+  getters: {
+    getErrors: (state) => state._errors,
+    isLoading: (state) => state._isLoading
+  },
+
   actions: {
+    async fetchErrors() {
+      this._isLoading = true
+      await getDocs(collection(db, 'errors'))
+        .then((querySnapshot) => {
+          const errors = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+          this.$patch({ _errors: errors })
+        })
+        .finally(() => (this._isLoading = false))
+    },
+
     async throwError(error, message) {
       const userStore = useUserStore()
       await userStore.fetchUserIp()
