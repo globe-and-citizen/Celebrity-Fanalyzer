55ab79c054be007e45686bc2d4668d2d2185ee62
Hermann EHO <hermannleboss@gmail.com>
2023-03-08 11:03:05 +0000

    test: enable navigation test

--- test/cypress/e2e/user-navigation.cy.js
+++ test/cypress/e2e/user-navigation.cy.js
@@ -3,24 +3,24 @@ describe('User Navigation', () => {
     cy.visit('/')
     cy.get('h2').contains('Welcome to Celebrity Fanalyzer!')
 
-    // cy.getByData('main-menu').find("a").eq(2).click()
-    // cy.location("pathname").should(
-    //   "eq",
-    //   "/month"
-    // )
-    // // cy.get("h2").contains("Entries")
-    //
-    // cy.getByData('main-menu').find("a").eq(1).click()
-    // cy.location("pathname").should(
-    //   "eq",
-    //   "/search"
-    // )
-    //
-    // cy.getByData('main-menu').find("a").eq(3).click()
-    // cy.location("pathname").should(
-    //   "eq",
-    //   "/profile"
-    // )
-    // cy.get("h1").contains("You are not logged in.")
+    cy.getByData('main-menu').find("a").eq(2).click()
+    cy.location("pathname").should(
+      "eq",
+      "/month"
+    )
+    // cy.get("h2").contains("Entries")
+
+    cy.getByData('main-menu').find("a").eq(1).click()
+    cy.location("pathname").should(
+      "eq",
+      "/search"
+    )
+
+    cy.getByData('main-menu').find("a").eq(3).click()
+    cy.location("pathname").should(
+      "eq",
+      "/profile"
+    )
+    cy.get("h1").contains("You are not logged in.")
   })
 })
