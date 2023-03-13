ed11ee1df4f731b8cd80d03e6b8fb53c067c3f17
Hermann EHO <hermannleboss@gmail.com>
2023-03-13 17:40:24 +0000

    test: remove month page checkout

--- test/cypress/e2e/user-navigation.cy.js
+++ test/cypress/e2e/user-navigation.cy.js
@@ -7,13 +7,6 @@ describe('User Navigation', () => {
         // "naturalWidth" and "naturalHeight" are set when the image loads
         expect($img[0].naturalWidth).to.be.greaterThan(0)
       })
-    cy.getByData('main-menu').find("a").eq(2).click()
-    // cy.wait(30000)
-    // cy.location("pathname", { timeout: 30000 }).should(
-    //   "eq",
-    //   "/month"
-    // )
-    // cy.get("h2").contains("Entries")
 
     cy.getByData('main-menu').find("a").eq(1).click()
     cy.location("pathname").should(
