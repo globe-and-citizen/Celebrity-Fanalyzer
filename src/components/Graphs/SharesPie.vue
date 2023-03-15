2cc972125171075882b86943092c6e59247af8eb
Arnon <arnonrdp@gmail.com>
2023-03-15 18:37:31 -0300

    Added `compute` function

--- src/components/Graphs/SharesPie.vue
+++ src/components/Graphs/SharesPie.vue
@@ -7,38 +7,49 @@ import { PieChart } from 'echarts/charts'
 import { LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components'
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
-import { ref } from 'vue'
+import { onBeforeUpdate, onMounted, ref } from 'vue'
 import VChart from 'vue-echarts'
 
 use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent])
 
 const props = defineProps({
-  data: { type: Array, required: true },
-  title: { type: String, required: false }
+  data: { type: Array, required: true }
 })
 
-const option = ref({
-  title: {
-    text: props.title,
-    left: 'center'
-  },
-  tooltip: {
-    trigger: 'item',
-    formatter: '{b}: {c} ({d}%)'
-  },
-  legend: {
-    orient: 'vertical',
-    left: 'left'
-  },
-  series: [
-    {
-      type: 'pie',
-      radius: '55%',
-      data: props.data,
-      colorBy: 'data',
-      color: ['#48982a', '#ea3423', '#f9a61a', '#2e7bb4', '#3ba272', '#fc8452', '#9a60b4', '#ea7ccc']
-    }
-  ]
+const option = ref({})
+
+function compute() {
+  option.value = {
+    title: {
+      text: 'Shares',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{b}: {c} ({d}%)'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        type: 'pie',
+        radius: '55%',
+        data: props.data,
+        colorBy: 'data',
+        color: ['#48982a', '#ea3423', '#f9a61a', '#2e7bb4', '#fc8452', '#9a60b4', '#ea7ccc']
+      }
+    ]
+  }
+}
+
+onBeforeUpdate(() => {
+  compute()
+})
+
+onMounted(() => {
+  compute()
 })
 </script>
 
