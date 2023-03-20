f20074f7010818be3b26cc82e5764a950e465b7c
Arnon <arnonrdp@gmail.com>
2023-03-20 15:40:28 -0300

    Fixed warning

--- src/stores/comments.js
+++ src/stores/comments.js
@@ -109,7 +109,7 @@ export const useCommentStore = defineStore('comments', {
         if (comment.id === commentId && !comment.likes?.includes(user)) {
           comment.likes.push(user)
           comment.dislikes = comment.dislikes.filter((user) => user !== user)
-        } else if (comment.likes.includes(user)) {
+        } else if (comment.likes?.includes(user)) {
           comment.likes = comment.likes.filter((user) => user !== user)
         }
         return comment
@@ -132,7 +132,7 @@ export const useCommentStore = defineStore('comments', {
         if (comment.id === id && !comment.dislikes?.includes(user)) {
           comment.dislikes.push(user)
           comment.likes = comment.likes.filter((user) => user !== user)
-        } else if (comment.dislikes.includes(user)) {
+        } else if (comment.dislikes?.includes(user)) {
           comment.dislikes = comment.dislikes.filter((user) => user !== user)
         }
         return comment
