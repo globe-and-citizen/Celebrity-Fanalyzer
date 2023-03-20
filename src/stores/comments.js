8cd4ee9e1e8369009ea2b8cdf80ca0607e6076df
NJavokhir <nematovjavohir58@gmail.com>
2023-03-20 23:23:48 +0500

    removed unnecessery function

--- src/stores/comments.js
+++ src/stores/comments.js
@@ -4,6 +4,7 @@ import {
   collection,
   deleteDoc,
   doc,
+  FieldValue,
   getDoc,
   getDocs,
   query,
@@ -105,11 +106,10 @@ export const useCommentStore = defineStore('comments', {
       await updateDoc(commentRef, { dislikes: arrayRemove(user) })
 
       const comments = this._comments.map((comment) => {
-        if (comment.id === commentId && !comment.likes.includes(user)) {
+        if (comment.id === commentId && !comment.likes?.includes(user)) {
           comment.likes.push(user)
           comment.dislikes = comment.dislikes.filter((user) => user !== user)
         } else if (comment.likes.includes(user)) {
-          updateDoc(commentRef, { likes: arrayRemove(user) })
           comment.likes = comment.likes.filter((user) => user !== user)
         }
         return comment
@@ -129,11 +129,10 @@ export const useCommentStore = defineStore('comments', {
       await updateDoc(commentRef, { likes: arrayRemove(user) })
 
       const comments = this._comments.map((comment) => {
-        if (comment.id === id && !comment.dislikes.includes(user)) {
+        if (comment.id === id && !comment.dislikes?.includes(user)) {
           comment.dislikes.push(user)
           comment.likes = comment.likes.filter((user) => user !== user)
         } else if (comment.dislikes.includes(user)) {
-          updateDoc(commentRef, { dislikes: arrayRemove(user) })
           comment.dislikes = comment.dislikes.filter((user) => user !== user)
         }
         return comment
