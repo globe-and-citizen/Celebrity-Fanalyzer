ad33ceac42aa53b9e7f7ecf55168ee06f3f1f26b
Arnon <arnonrdp@gmail.com>
2023-04-06 18:36:39 -0300

    Removed entry state and function

--- src/stores/entries.js
+++ src/stores/entries.js
@@ -20,32 +20,16 @@ import { useCommentStore, useErrorStore, useLikeStore, usePromptStore, useShareS
 
 export const useEntryStore = defineStore('entries', {
   state: () => ({
-    _entries: [],
     _isLoading: false
   }),
 
   persist: true,
 
   getters: {
-    getEntries: (state) => state._entries,
     isLoading: (state) => state._isLoading
   },
 
   actions: {
-    async fetchAllEntries() {
-      this._isLoading = true
-      const querySnapshot = await getDocs(collection(db, 'entries'))
-      const entries = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-
-      for (const entry of entries) {
-        entry.author = await getDoc(entry.author).then((doc) => doc.data())
-      }
-
-      this._entries = []
-      this.$patch({ _entries: entries })
-      this._isLoading = false
-    },
-
     async fetchEntryBySlug(slug) {
       this._isLoading = true
       const querySnapshot = await getDocs(query(collection(db, 'entries'), where('slug', '==', slug)))
