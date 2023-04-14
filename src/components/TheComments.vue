0184d667e37eba38b5e4153624362306919ff66f
NJavokhir <nematovjavohir58@gmail.com>
2023-04-14 20:11:09 +0500

    improved code with prettie

--- src/components/TheComments.vue
+++ src/components/TheComments.vue
@@ -8,7 +8,9 @@
             <q-img :src="comment.author.photoURL" />
           </q-avatar>
           <p class="column q-mb-none q-ml-sm">
-            <span class="text-body2">{{ comment.author.displayName || 'Anonymous' }}</span>
+            <span class="text-body2">{{
+              comment.author.displayName || "Anonymous"
+            }}</span>
             <span class="text-body2 text-secondary">
               {{ shortMonthDayTime(comment.created) }}
             </span>
@@ -24,12 +26,22 @@
             :data-test="comment.text + '-button-dropdown'"
           >
             <q-list>
-              <q-item data-test="comment-select-edit" clickable v-close-popup @click="editInput(comment.id)">
+              <q-item
+                data-test="comment-select-edit"
+                clickable
+                v-close-popup
+                @click="editInput(comment.id)"
+              >
                 <q-item-section>
                   <q-item-label>Edit</q-item-label>
                 </q-item-section>
               </q-item>
-              <q-item data-test="comment-select-delete" clickable v-close-popup @click="deleteComment(1, comment.id)">
+              <q-item
+                data-test="comment-select-delete"
+                clickable
+                v-close-popup
+                @click="deleteComment(1, comment.id)"
+              >
                 <q-item-section>
                   <q-item-label>Delete</q-item-label>
                 </q-item-section>
@@ -37,7 +49,11 @@
             </q-list>
           </q-btn-dropdown>
         </div>
-        <q-form v-if="isEditing && comment.id === inputEdit" greedy @submit.prevent="editComment(comment.id, comment.text)">
+        <q-form
+          v-if="isEditing && comment.id === inputEdit"
+          greedy
+          @submit.prevent="editComment(comment.id, comment.text)"
+        >
           <q-input
             :data-test="comment.text + '-comment-edit'"
             class="q-px-sm"
@@ -51,7 +67,15 @@
             standout="bg-secondary text-white"
             v-model="comment.text"
           >
-            <q-btn data-test="submit-edited-comment" class="cursor-pointer" color="grey-6" flat icon="send" round type="submit" />
+            <q-btn
+              data-test="submit-edited-comment"
+              class="cursor-pointer"
+              color="grey-6"
+              flat
+              icon="send"
+              round
+              type="submit"
+            />
           </q-input>
         </q-form>
         <div v-else class="q-my-sm text-body2">
@@ -61,7 +85,11 @@
           <q-btn
             :data-test="'like' + comment.text"
             flat
-            :icon="likeIconClass(comment) ? 'img:/icons/thumbs-up-bolder.svg' : 'img:/icons/thumbs-up.svg'"
+            :icon="
+              likeIconClass(comment)
+                ? 'img:/icons/thumbs-up-bolder.svg'
+                : 'img:/icons/thumbs-up.svg'
+            "
             :label="comment.likes?.length || 0"
             rounded
             size="0.75rem"
@@ -72,7 +100,11 @@
           <q-btn
             :data-test="'dislike' + comment.text"
             flat
-            :icon="dislikeIconClass(comment) ? 'img:/icons/thumbs-down-bolder.svg' : 'img:/icons/thumbs-down.svg'"
+            :icon="
+              dislikeIconClass(comment)
+                ? 'img:/icons/thumbs-down-bolder.svg'
+                : 'img:/icons/thumbs-down.svg'
+            "
             :label="comment.dislikes?.length || 0"
             rounded
             size="0.75rem"
@@ -99,15 +131,23 @@
             </div>
             <q-form v-else greedy @submit.prevent="addReply(comment.id)">
               <!-- Started Child Comment section -->
-              <div v-for="childComment of childComments" class="q-mb-md" :key="childComment.id">
+              <div
+                v-for="childComment of childComments"
+                class="q-mb-md"
+                :key="childComment.id"
+              >
                 <div class="flex items-center">
                   <q-icon v-if="childComment.isAnonymous" name="person" size="1.5rem" />
                   <q-avatar v-else size="1rem">
                     <q-img :src="childComment.author.photoURL" />
                   </q-avatar>
                   <p class="row q-mb-none q-ml-sm">
-                    <span class="text-body2 q-mr-sm">{{ childComment.author.displayName || 'Anonymous' }}</span>
-                    <span class="text-caption text-secondary">{{ shortMonthDayTime(childComment.created) }}</span>
+                    <span class="text-body2 q-mr-sm">{{
+                      childComment.author.displayName || "Anonymous"
+                    }}</span>
+                    <span class="text-caption text-secondary">{{
+                      shortMonthDayTime(childComment.created)
+                    }}</span>
                   </p>
                   <q-space />
                   <q-btn-dropdown
@@ -157,7 +197,9 @@
                     lazy-rules
                     required
                     rounded
-                    :rules="[(val) => val.length > 1 || 'Please type at least 2 characters']"
+                    :rules="[
+                      (val) => val.length > 1 || 'Please type at least 2 characters',
+                    ]"
                     standout="bg-secondary text-white"
                     v-model="childComment.text"
                   >
@@ -222,135 +264,147 @@
       style="margin-bottom: 6.7rem"
       v-model="myComment.text"
     >
-      <q-btn data-test="submit-comment" class="cursor-pointer" color="grey-6" flat icon="send" round type="submit" />
+      <q-btn
+        data-test="submit-comment"
+        class="cursor-pointer"
+        color="grey-6"
+        flat
+        icon="send"
+        round
+        type="submit"
+      />
     </q-input>
   </q-form>
 </template>
 
 <script setup>
-import { useQuasar } from 'quasar'
-import { useCommentStore, useErrorStore, useUserStore } from 'src/stores'
-import { shortMonthDayTime } from 'src/utils/date'
-import { computed, onMounted, reactive, ref } from 'vue'
+import { useQuasar } from "quasar";
+import { useCommentStore, useErrorStore, useUserStore } from "src/stores";
+import { shortMonthDayTime } from "src/utils/date";
+import { computed, onMounted, reactive, ref } from "vue";
 
 const props = defineProps({
   comments: { type: Array, required: true },
-  entry: { type: Object, required: true }
-})
+  entry: { type: Object, required: true },
+});
 
-const $q = useQuasar()
-const commentStore = useCommentStore()
-const errorStore = useErrorStore()
-const userStore = useUserStore()
+const $q = useQuasar();
+const commentStore = useCommentStore();
+const errorStore = useErrorStore();
+const userStore = useUserStore();
 
-const childComments = ref([])
-const commentId = ref('')
-const expanded = ref(false)
-const inputEdit = ref('')
-const isEditing = ref(false)
-const myComment = reactive({})
-const reply = reactive({})
-const user = ref('')
-const userId = ref('')
+const childComments = ref([]);
+const commentId = ref("");
+const expanded = ref(false);
+const inputEdit = ref("");
+const isEditing = ref(false);
+const myComment = reactive({});
+const reply = reactive({});
+const user = ref("");
+const userId = ref("");
 
 onMounted(async () => {
-  await userStore.fetchUserIp()
-  userId.value = userStore.getUserRef?.id || userStore.getUserIpHash
+  await userStore.fetchUserIp();
+  userId.value = userStore.getUserRef?.id || userStore.getUserIpHash;
 
-  user.value = userStore.getUserRef || userStore.getUserIpHash
-})
+  user.value = userStore.getUserRef || userStore.getUserIpHash;
+});
 
 const likeIconClass = computed(() => {
   return (comment) => {
-    return comment.likes ? comment.likes.map((item) => item.id).includes(user.value.id) : false
-  }
-})
+    return comment.likes
+      ? comment.likes.map((item) => item.id).includes(user.value.id)
+      : false;
+  };
+});
 
 const dislikeIconClass = computed(() => {
   return (comment) => {
-    return comment.dislikes ? comment.dislikes.some((dislike) => dislike.id === user.value.id) : false
-  }
-})
+    return comment.dislikes
+      ? comment.dislikes.some((dislike) => dislike.id === user.value.id)
+      : false;
+  };
+});
 
 const replyCounter = (id) => {
-  let count = 0
+  let count = 0;
   for (const comment of props.comments) {
     if (id === comment.parentId) {
-      count++
+      count++;
     }
   }
-  return count
-}
+  return count;
+};
 
 async function addComment() {
   await commentStore
     .addComment(myComment, props.entry)
     .then(() => {
-      myComment.text = ''
-      window.scrollTo(0, document.body.scrollHeight)
-      $q.notify({ type: 'positive', message: 'Comment successfully submitted' })
+      myComment.text = "";
+      window.scrollTo(0, document.body.scrollHeight);
+      $q.notify({ type: "positive", message: "Comment successfully submitted" });
     })
-    .catch((error) => errorStore.throwError(error, 'Comment submission failed!'))
+    .catch((error) => errorStore.throwError(error, "Comment submission failed!"));
 }
 
 function likeComment(commentId) {
-  commentStore.likeComment(props.entry.id, commentId)
+  commentStore.likeComment(props.entry.id, commentId);
 }
 
 function dislikeComment(commentId) {
-  commentStore.dislikeComment(props.entry.id, commentId)
+  commentStore.dislikeComment(props.entry.id, commentId);
 }
 
 function editInput(commentId) {
-  isEditing.value = !isEditing.value
-  inputEdit.value = commentId
+  isEditing.value = !isEditing.value;
+  inputEdit.value = commentId;
 }
 
 async function editComment(commentId, editedComment) {
   await commentStore
     .editComment(props.entry.id, commentId, editedComment, userId.value)
-    .then(() => $q.notify({ type: 'info', message: 'Comment successfully edited!' }))
-    .catch(() => errorStore.throwError(error, 'Failed to edit comment'))
-    .finally(() => (isEditing.value = false))
+    .then(() => $q.notify({ type: "info", message: "Comment successfully edited!" }))
+    .catch(() => errorStore.throwError(error, "Failed to edit comment"))
+    .finally(() => (isEditing.value = false));
 }
 
 async function deleteComment(commentParentId, commentId) {
   await commentStore
     .deleteComment(props.entry.id, commentId)
-    .then(() => $q.notify({ type: 'negative', message: 'Comment successfully deleted' }))
-    .catch((error) => errorStore.throwError(error, 'Failed to delete comment'))
+    .then(() => $q.notify({ type: "negative", message: "Comment successfully deleted" }))
+    .catch((error) => errorStore.throwError(error, "Failed to delete comment"));
 
-  childComments.value = []
+  childComments.value = [];
   for (const comment of props.comments) {
     if (commentParentId === comment.parentId) {
-      childComments.value.push(comment)
+      childComments.value.push(comment);
     } else {
-      continue
+      continue;
     }
   }
 }
 
 async function replyInput(parentId) {
-  reply.parentId = parentId
+  reply.parentId = parentId;
 }
 
 async function showReplies(id) {
-  childComments.value = []
+  childComments.value = [];
   if (commentId.value === id) {
-    expanded.value = false
-    commentId.value = ''
-    return
+    expanded.value = false;
+    commentId.value = "";
+    return;
   }
-  expanded.value = true
-  commentId.value = id
+  expanded.value = true;
+  commentId.value = id;
 
-  reply.parentId = id
+  reply.parentId = id;
 
   for (const comment of props.comments) {
     if (id === comment.parentId) {
-      childComments.value.push(comment)
+      childComments.value.push(comment);
     } else {
-      continue
+      continue;
     }
   }
 }
@@ -359,17 +413,17 @@ async function addReply(commentId) {
   await commentStore
     .addReply(props.entry.id, commentId, reply)
     .then(() => {
-      reply.text = ''
-      $q.notify({ type: 'positive', message: 'Reply successfully submitted' })
+      reply.text = "";
+      $q.notify({ type: "positive", message: "Reply successfully submitted" });
     })
-    .catch((error) => errorStore.throwError(error, 'Reply submission failed!'))
+    .catch((error) => errorStore.throwError(error, "Reply submission failed!"));
 
-  childComments.value = []
+  childComments.value = [];
   for (const comment of props.comments) {
     if (commentId === comment.parentId) {
-      childComments.value.push(comment)
+      childComments.value.push(comment);
     } else {
-      continue
+      continue;
     }
   }
 }
@@ -377,11 +431,11 @@ async function addReply(commentId) {
 
 <style scoped>
 .bolder-icon-default {
-  font-variation-settings: 'FILL' 0, 'wght' 300, 'GRAD' 0, 'opsz' 32;
+  font-variation-settings: "FILL" 0, "wght" 300, "GRAD" 0, "opsz" 32;
 }
 
 .bolder-icon {
-  font-variation-settings: 'FILL' 1, 'wght' 300, 'GRAD' 0, 'opsz' 32;
+  font-variation-settings: "FILL" 1, "wght" 300, "GRAD" 0, "opsz" 32;
 }
 
 .warning-icon {
