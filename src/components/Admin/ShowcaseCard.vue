a5de2db902019473642a891bfc82d1fccc97818e
Arnon <arnonrdp@gmail.com>
2023-04-22 15:59:28 -0300

    Added `q-uploader`

--- src/components/Admin/ShowcaseCard.vue
+++ src/components/Admin/ShowcaseCard.vue
@@ -1,5 +1,17 @@
 <template>
-  <h1>Hello World</h1>
+  <section class="flex justify-center">
+    <q-uploader accept="image/*" bordered class="full-width" multiple flat style="max-height: 32rem" />
+  </section>
 </template>
 
 <script setup></script>
+
+<style>
+.q-uploader__list {
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+}
+</style>
