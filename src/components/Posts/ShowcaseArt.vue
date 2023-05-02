ec1a3371e51811a39fc5c8bb2b5ce3c4ba257921
Arnon <arnonrdp@gmail.com>
2023-05-02 18:18:16 -0300

    Added art into Dialog

--- src/components/Posts/ShowcaseArt.vue
+++ src/components/Posts/ShowcaseArt.vue
@@ -1,30 +1,35 @@
 <template>
-  <q-carousel
-    animated
-    arrows
-    control-color="primary"
-    height="auto"
-    navigation-active-icon="radio_button_checked"
-    navigation-icon="radio_button_unchecked"
-    navigation
-    padding
-    swipeable
-    transition-prev="jump-right"
-    transition-next="jump-left"
-    v-model="slide"
-  >
-    <q-carousel-slide v-for="(art, index) in showcase?.arts" :key="index" :name="index">
-      <q-img class="rounded-borders" fit="contain" :src="art" style="max-height: 20rem" />
-    </q-carousel-slide>
-    <q-carousel-slide :name="showcase?.arts.length + 1">
-      <q-card flat>
-        <q-card-section class="row" style="white-space: pre-line">
-          <q-img class="col-6 rounded-borders" :src="showcase.artist.photo" />
-          <p class="col-6 q-px-md">{{ showcase.artist.info }}</p>
-        </q-card-section>
+  <q-section class="q-pa-md">
+    <q-img class="cursor-pointer" fit="contain" :src="showcase?.arts[0]" width="50%" @click="openDialog = true" />
+    <q-dialog position="top" v-model="openDialog">
+      <q-card style="width: 50rem; max-width: 90vw">
+        <q-carousel
+          animated
+          control-color="primary"
+          height="50vh"
+          navigation-active-icon="radio_button_checked"
+          navigation-icon="radio_button_unchecked"
+          navigation
+          swipeable
+          transition-prev="jump-right"
+          transition-next="jump-left"
+          v-model="slide"
+        >
+          <q-carousel-slide v-for="(art, index) in showcase?.arts" class="flex justify-center q-pa-none" :key="index" :name="index">
+            <q-img class="rounded-borders" fit="contain" :src="art" />
+          </q-carousel-slide>
+          <q-carousel-slide class="flex items-center justify-center" :name="showcase?.arts.length + 1">
+            <q-card flat>
+              <q-card-section class="row" style="white-space: pre-line">
+                <q-img class="col-sm-6 col-xs-12 rounded-borders" :src="showcase.artist.photo" />
+                <p class="col-sm-6 col-xs-12 q-px-md">{{ showcase.artist.info }}</p>
+              </q-card-section>
+            </q-card>
+          </q-carousel-slide>
+        </q-carousel>
       </q-card>
-    </q-carousel-slide>
-  </q-carousel>
+    </q-dialog>
+  </q-section>
 </template>
 
 <script setup>
@@ -34,5 +39,6 @@ defineProps({
   showcase: { type: Object, required: true, default: () => {} }
 })
 
+const openDialog = ref(false)
 const slide = ref(0)
 </script>
