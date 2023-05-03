04b2aee0a7a3942e06be0f425d6f76df1401fb0f
Arnon <arnonrdp@gmail.com>
2023-05-03 16:24:04 -0300

    Improvement in the display of the arts

--- src/components/Posts/ShowcaseArt.vue
+++ src/components/Posts/ShowcaseArt.vue
@@ -1,30 +1,40 @@
 <template>
-  <section class="q-pa-md">
-    <q-img class="cursor-pointer" fit="contain" :src="showcase?.arts[0]" width="50%" @click="openDialog = true" />
-    <q-dialog position="top" v-model="openDialog">
-      <q-carousel
-        animated
-        control-color="primary"
-        height="50vh"
-        navigation-active-icon="radio_button_checked"
-        navigation-icon="radio_button_unchecked"
-        navigation
-        style="width: 50rem; max-width: 90vw"
-        swipeable
-        transition-prev="jump-right"
-        transition-next="jump-left"
-        v-model="slide"
-      >
-        <q-carousel-slide v-for="(art, index) in showcase?.arts" class="flex justify-center q-pa-none" :key="index" :name="index">
-          <q-img class="rounded-borders" fit="contain" :src="art" />
-        </q-carousel-slide>
-        <q-carousel-slide class="q-pa-none row" :name="showcase?.arts.length + 1">
-          <q-img class="col-sm-6 col-xs-12 rounded-borders" :src="showcase.artist.photo" />
-          <p class="col-sm-6 col-xs-12 flex items-center q-pa-md" style="white-space: pre-line">{{ showcase.artist.info }}</p>
-        </q-carousel-slide>
-      </q-carousel>
-    </q-dialog>
-  </section>
+  <div class="q-gutter-xs q-pa-md text-center" @click="openDialog = true">
+    <q-img
+      v-for="(art, index) in showcase?.arts"
+      img-class="art-img"
+      fit="cover"
+      :key="index"
+      :ratio="1"
+      :src="art"
+      width="8rem"
+      @click="slide = index"
+    />
+  </div>
+
+  <q-dialog position="top" v-model="openDialog">
+    <q-carousel
+      animated
+      control-color="primary"
+      height="50vh"
+      navigation-active-icon="radio_button_checked"
+      navigation-icon="radio_button_unchecked"
+      navigation
+      style="width: 50rem; max-width: 90vw"
+      swipeable
+      transition-prev="jump-right"
+      transition-next="jump-left"
+      v-model="slide"
+    >
+      <q-carousel-slide v-for="(art, index) in showcase?.arts" class="flex justify-center q-pa-none" :key="index" :name="index">
+        <q-img class="rounded-borders" fit="contain" :src="art" />
+      </q-carousel-slide>
+      <q-carousel-slide class="q-pa-none row" :name="showcase?.arts.length + 1">
+        <q-img class="col-sm-6 col-xs-12 rounded-borders" :src="showcase.artist.photo" />
+        <p class="col-sm-6 col-xs-12 flex items-center q-pa-md" style="white-space: pre-line">{{ showcase.artist.info }}</p>
+      </q-carousel-slide>
+    </q-carousel>
+  </q-dialog>
 </template>
 
 <script setup>
@@ -37,3 +47,15 @@ defineProps({
 const openDialog = ref(false)
 const slide = ref(0)
 </script>
+
+<style>
+.art-img {
+  cursor: pointer;
+  filter: grayscale(100%);
+  transition: filter 0.3s ease-in-out;
+}
+
+.art-img:hover {
+  filter: grayscale(0%);
+}
+</style>
