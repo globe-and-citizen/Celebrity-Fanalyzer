8e5cf61f66892b7ec01e66401b67e59345d08b21
Arnon <arnonrdp@gmail.com>
2023-05-10 16:07:52 -0300

    Added animation for the phrase

--- quasar.config.js
+++ quasar.config.js
@@ -163,7 +163,7 @@ module.exports = configure(function (ctx) {
 
     // animations: 'all', // --- includes all animations
     // https://v2.quasar.dev/options/animations
-    animations: [],
+    animations: ['slideInDown'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#property-sourcefiles
     sourceFiles: {
