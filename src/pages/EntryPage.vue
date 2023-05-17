5058522d4011cc849962b3b4c340e90295236391
Arnon <arnonrdp@gmail.com>
2023-05-17 16:55:06 -0300

    Reused `TheAnthrogram` to replace second tab of Entries

--- src/pages/EntryPage.vue
+++ src/pages/EntryPage.vue
@@ -12,33 +12,7 @@
     </q-tab-panel>
     <!-- Panel 2: Anthrogram -->
     <q-tab-panel name="stats" class="bg-white">
-      <TheHeader title="Anthrogram" />
-      <q-page>
-        <section>
-          <h1 class="q-mt-none text-bold text-h5">{{ entry?.title }}</h1>
-          <div v-if="entry.author" class="flex no-wrap items-center q-mb-xl">
-            <q-avatar size="4rem">
-              <img :src="entry.author.photoURL" alt="Author Image" />
-            </q-avatar>
-            <p class="q-mb-none q-ml-md text-h5">{{ entry.author.displayName }}</p>
-          </div>
-          <q-tabs
-            active-color="primary"
-            align="justify"
-            class="text-grey q-mb-xl"
-            dense
-            indicator-color="primary"
-            narrow-indicator
-            v-model="type"
-          >
-            <q-tab name="day" label="Days" />
-            <q-tab name="week" label="Week" />
-            <q-tab name="all" label="All" />
-          </q-tabs>
-          <LikesBar :data="graphData(type)" />
-          <SharesPie :data="shares" :interval="type" />
-        </section>
-      </q-page>
+      <TheAnthrogram :post="entry" />
     </q-tab-panel>
     <!-- Panel 3: Comments -->
     <q-tab-panel name="comments" class="bg-white">
@@ -51,15 +25,11 @@
 </template>
 
 <script setup>
-import { Timestamp } from 'firebase/firestore'
-import LikesBar from 'src/components/Graphs/LikesBar.vue'
-import SharesPie from 'src/components/Graphs/SharesPie.vue'
+import TheAnthrogram from 'src/components/Posts/TheAnthrogram.vue'
 import TheComments from 'src/components/Posts/TheComments.vue'
 import ThePost from 'src/components/Posts/ThePost.vue'
 import TheHeader from 'src/components/TheHeader.vue'
-import { useCommentStore, useEntryStore, useErrorStore, useLikeStore, useShareStore, useUserStore } from 'src/stores'
-import { getStats } from 'src/utils/date'
-import { formatAllStats, formatDayStats, formatWeekStats } from 'src/utils/stats'
+import { useCommentStore, useEntryStore, useErrorStore, useLikeStore, useShareStore } from 'src/stores'
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -70,14 +40,10 @@ const errorStore = useErrorStore()
 const entryStore = useEntryStore()
 const likeStore = useLikeStore()
 const shareStore = useShareStore()
-const userStore = useUserStore()
 
-const chartData = ref({})
 const comments = ref([])
 const entry = ref({})
-const shares = ref([])
 const tab = ref('entry')
-const type = ref('day')
 
 onMounted(async () => {
   if (router.currentRoute.value.params.id) {
@@ -97,51 +63,15 @@ onMounted(async () => {
 
   await likeStore.getAllLikesDislikes('entries', entry.value.id).catch((error) => errorStore.throwError(error))
 
-  await shareStore
-    .fetchShares('entries', entry.value.id)
-    .then(() => (shares.value = shareStore.getShares))
-    .catch((error) => errorStore.throwError(error))
+  await shareStore.fetchShares('entries', entry.value.id).catch((error) => errorStore.throwError(error))
 })
-
-function graphData(type) {
-  if (type === 'day') {
-    return formatDayStats(chartData.value.dayStats)
-  }
-  if (type === 'week') {
-    return formatWeekStats(chartData.value.weekStats)
-  }
-  return formatAllStats(chartData.value.allStats)
-}
 
 commentStore.$subscribe((_mutation, state) => {
   comments.value = state._comments
 })
-
-likeStore.$subscribe((_mutation, state) => {
-  if (!entry.value.created) return
-  const { weekStats, dayStats } = getStats(state, entry.value.created)
-  const allStats = [
-    {
-      date: Timestamp.fromDate(new Date()),
-      likes: state._likes.length,
-      dislikes: state._dislikes.length
-    }
-  ]
-  chartData.value = { weekStats, dayStats, allStats }
-})
-
-shareStore.$subscribe((_mutation, state) => {
-  shares.value = state._shares
-})
 </script>
 
 <style scoped lang="scss">
-.parallax {
-  position: fixed;
-  top: 65px;
-  z-index: -1;
-}
-
 .tab-selector {
   margin-bottom: 4rem;
   z-index: 3;
