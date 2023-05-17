456bbd34d637569227fb386937f9fe79ae54630f
Arnon <arnonrdp@gmail.com>
2023-05-17 16:33:08 -0300

    Reused `TheAnthrogram` to replace second tab of Prompts

--- src/pages/PromptPage.vue
+++ src/pages/PromptPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-tabs active-color="primary" class="bg-white fixed-bottom tab-selector" dense indicator-color="transparent" v-model="tab">
     <q-tab content-class="q-ml-auto q-pb-md" data-test="prompt-tab" icon="fiber_manual_record" name="prompt" :ripple="false" />
-    <q-tab content-class="q-pb-md" data-test="graph-tab" icon="fiber_manual_record" name="stats" :ripple="false" />
+    <q-tab content-class="q-pb-md" data-test="graph-tab" icon="fiber_manual_record" name="anthrogram" :ripple="false" />
     <q-tab content-class="q-mr-auto q-pb-md" data-test="comments-tab" icon="fiber_manual_record" name="comments" :ripple="false" />
   </q-tabs>
   <q-spinner v-if="!Object.keys(prompt).length && promptStore.isLoading" class="absolute-center" color="primary" size="3em" />
@@ -12,35 +12,8 @@
       <TheEntries :entries="prompt?.entries" />
     </q-tab-panel>
     <!-- Panel 2: Anthrogram -->
-    <q-tab-panel name="stats" class="bg-white">
-      <TheHeader title="Anthrogram" />
-      <q-page>
-        <section>
-          <h1 class="q-mt-none text-bold text-h5">{{ prompt?.title }}</h1>
-          <div v-if="prompt?.author" class="flex no-wrap items-center q-mb-xl">
-            <q-avatar size="4rem">
-              <img :src="prompt.author.photoURL" alt="" />
-            </q-avatar>
-            <p class="q-mb-none q-ml-md text-h6 text-weight-light">{{ prompt.author.displayName }}</p>
-          </div>
-
-          <q-tabs
-            active-color="primary"
-            align="justify"
-            class="text-grey q-mb-xl"
-            dense
-            indicator-color="primary"
-            narrow-indicator
-            v-model="type"
-          >
-            <q-tab name="day" label="Days" />
-            <q-tab name="week" label="Week" />
-            <q-tab name="all" label="All" />
-          </q-tabs>
-          <LikesBar :data="graphData(type)" />
-          <SharesPie :data="shares" :interval="type" />
-        </section>
-      </q-page>
+    <q-tab-panel name="anthrogram" class="bg-white">
+      <TheAnthrogram :post="prompt" />
     </q-tab-panel>
     <!-- Panel 3: Comments -->
     <q-tab-panel name="comments" class="bg-white">
@@ -53,16 +26,13 @@
 </template>
 
 <script setup>
-import { Timestamp } from 'firebase/firestore'
-import LikesBar from 'src/components/Graphs/LikesBar.vue'
-import SharesPie from 'src/components/Graphs/SharesPie.vue'
+import TheAnthrogram from 'src/components/Posts/TheAnthrogram.vue'
 import TheComments from 'src/components/Posts/TheComments.vue'
 import ThePost from 'src/components/Posts/ThePost.vue'
 import TheEntries from 'src/components/TheEntries.vue'
 import TheHeader from 'src/components/TheHeader.vue'
 import { useCommentStore, useEntryStore, useErrorStore, useLikeStore, usePromptStore, useShareStore } from 'src/stores'
-import { currentYearMonth, getStats, previousYearMonth } from 'src/utils/date'
-import { formatAllStats, formatDayStats, formatWeekStats } from 'src/utils/stats'
+import { currentYearMonth, previousYearMonth } from 'src/utils/date'
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -74,26 +44,12 @@ const errorStore = useErrorStore()
 const likeStore = useLikeStore()
 const promptStore = usePromptStore()
 const shareStore = useShareStore()
-
-const chartData = ref({})
 const comments = ref([])
 const countComments = ref(0)
 const prompt = ref({})
-const shares = ref([])
 const tab = ref('prompt')
-const type = ref('all')
 const shareIsLoading = ref(false)
 const shareIsLoaded = ref(false)
-
-function graphData(type) {
-  if (type === 'day') {
-    return formatDayStats(chartData.value.dayStats)
-  }
-  if (type === 'week') {
-    return formatWeekStats(chartData.value.weekStats)
-  }
-  return formatAllStats(chartData.value.allStats)
-}
 
 onMounted(async () => {
   if (!promptByRoute()) {
@@ -122,7 +78,6 @@ onMounted(async () => {
   shareIsLoading.value = true
   await shareStore
     .fetchShares('prompts', prompt.value.id)
-    .then(() => (shares.value = shareStore.getShares))
     .catch((error) => errorStore.throwError(error))
     .finally(() => {
       shareIsLoading.value = false
@@ -151,23 +106,6 @@ const promptByRoute = () => {
 
 commentStore.$subscribe((_mutation, state) => {
   comments.value = state._comments
-})
-
-likeStore.$subscribe((_mutation, state) => {
-  if (!prompt.value.created) return
-  const { weekStats, dayStats } = getStats(state, prompt.value.created)
-  const allStats = [
-    {
-      date: Timestamp.fromDate(new Date()),
-      likes: state._likes.length,
-      dislikes: state._dislikes.length
-    }
-  ]
-  chartData.value = { weekStats, dayStats, allStats }
-})
-
-shareStore.$subscribe((_mutation, state) => {
-  shares.value = state._shares
 })
 </script>
 
