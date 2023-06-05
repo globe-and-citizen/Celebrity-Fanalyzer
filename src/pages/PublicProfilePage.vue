2172f564362f1c3f2b2891798330bb536758ee01
Arnon <arnonrdp@gmail.com>
2023-06-05 18:12:41 -0300

    Added social links to profile

--- src/pages/PublicProfilePage.vue
+++ src/pages/PublicProfilePage.vue
@@ -9,9 +9,46 @@
           </q-avatar>
           <h5 class="q-my-none">{{ user.displayName }}</h5>
           <div class="col-grow text-right">
-            Social Networks
-            <br />
-            will be placed here...
+            <q-btn
+              v-if="user.facebook"
+              flat
+              :href="`https://facebook.com/${user.facebook}`"
+              icon="img:/icons/facebook.svg"
+              round
+              target="_blank"
+            />
+            <q-btn
+              v-if="user.instagram"
+              flat
+              :href="`https://instagram.com/${user.instagram}`"
+              icon="img:/icons/instagram.svg"
+              round
+              target="_blank"
+            />
+            <q-btn
+              v-if="user.linkedin"
+              flat
+              :href="`https://linkedin.com/in/${user.linkedin}`"
+              icon="img:/icons/linkedin.svg"
+              round
+              target="_blank"
+            />
+            <q-btn
+              v-if="user.telegram"
+              flat
+              :href="`https://telegram.com/${user.telegram}`"
+              icon="img:/icons/telegram.svg"
+              round
+              target="_blank"
+            />
+            <q-btn
+              v-if="user.twitter"
+              flat
+              :href="`https://twitter.com/${user.twitter}`"
+              icon="img:/icons/twitter.svg"
+              round
+              target="_blank"
+            />
           </div>
         </q-card-section>
         <q-card-section>
