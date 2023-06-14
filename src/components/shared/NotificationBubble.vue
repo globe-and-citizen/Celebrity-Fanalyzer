5bc9c7388d7e9e8ee4be2e479471d34486a890e2
Arnon <arnonrdp@gmail.com>
2023-06-14 16:37:27 -0300

    Added badges for unread notifications

--- src/components/shared/NotificationBubble.vue
+++ src/components/shared/NotificationBubble.vue
@@ -1,9 +1,10 @@
 <template>
-  <q-btn color="secondary" flat icon="notifications" round size="1rem">
+  <q-btn color="secondary" dense flat icon="notifications" round size="1rem">
+    <q-badge color="red" floating rounded>{{ unreadedNotifications }}</q-badge>
     <q-menu anchor="bottom right" self="top right">
-      <q-item>
+      <q-item style="min-width: 320px">
         <q-item-section class="text-center">
-          <q-btn color="primary" dense label="Mark all as read" outline @click="readAll" />
+          <q-btn class="no-wrap" color="primary" dense label="Mark all as read" outline @click="readAll" />
         </q-item-section>
         <q-item-section>
           <q-btn color="primary" dense label="Clear all" outline @click="clearAll" />
@@ -11,7 +12,13 @@
       </q-item>
       <q-item v-for="notification in notificationStore.getNotifications" class="non-selectable" :key="notification.id">
         <q-item-section side>
-          <q-icon class="cursor-pointer" color="blue" name="circle" size="0.75rem" @click="toggleRead" />
+          <q-icon
+            class="cursor-pointer"
+            color="blue"
+            :name="notification.read ? 'radio_button_unchecked' : 'circle'"
+            size="0.75rem"
+            @click="toggleRead"
+          />
         </q-item-section>
         <q-item-section class="cursor-pointer" @click="goToLink(notification.link)">{{ notification.message }}</q-item-section>
         <q-item-section side>
@@ -24,18 +31,18 @@
 
 <script setup>
 import { useNotificationStore } from 'app/src/stores'
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
 const notificationStore = useNotificationStore()
 
-onMounted(() => {
-  if (!notificationStore.getNotifications.length) {
-    notificationStore.readNotifications()
-  }
+onMounted(async () => {
+  await notificationStore.readNotifications()
 })
+
+const unreadedNotifications = computed(() => notificationStore.getNotifications.filter((notification) => !notification.read).length)
 
 function toggleRead() {
   // notificationStore.toggleRead()
