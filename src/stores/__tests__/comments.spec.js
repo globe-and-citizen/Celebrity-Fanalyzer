a967ba47a1275d8a8f4c0a226a06924050aec6aa
Hermann EHO <hermannleboss@gmail.com>
2023-06-16 13:26:30 +0000

    test: fix comments spec

--- src/stores/__tests__/comments.spec.js
+++ src/stores/__tests__/comments.spec.js
@@ -5,22 +5,13 @@ import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest'
 // Necessary Components
 import { useCommentStore, useEntryStore, useUserStore } from 'src/stores'
 import { ref, reactive } from 'vue'
-
-// Snapshot Listener Helper -- A Shameless Hack
-async function letSnapshotListenerRun(delay) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res()
-    }, delay)
-  })
-}
+import { waitUntil } from 'src/waitUntil'
 
 describe('Comments Store', () => {
-  setActivePinia(createPinia())
-  const entryStore = useEntryStore()
-  const commentStore = useCommentStore()
 
   beforeEach(async () => {
+    // Put setActivePinia in beforeEach beacause we need a fresh store
+    setActivePinia(createPinia())
     // In the Pinia store user.js, the call to fetch to get the user IP breaks. This is a mock to prevent breaking.
     global.fetch = vi.fn(async () => {
       return {
@@ -45,45 +36,64 @@ describe('Comments Store', () => {
     }
   })
 
+
   it('Create and then delete a fake comment in here', async () => {
     // Step 1: Retrieve an entry to comment on.
+
+    const entryStore = useEntryStore()
+    const commentStore = useCommentStore()
     const firstEntry = ref({})
     await entryStore.fetchEntries()
+    await waitUntil(() => {
+
+      // TODO : Default state : find a better way to test it. Should use undefined for default state
+      return entryStore.getEntries.length > 0
+    })
     firstEntry.value = entryStore.getEntries[0]
 
     // Step 2: Check the starting number of comments.
     await commentStore.fetchComments('entries', firstEntry.value.id)
-    await letSnapshotListenerRun(500)
+    // Example usage
+    await waitUntil(() => {
+      // TODO : Default state
+      return commentStore.getComments.length > 0
+    })
     const startingNumberOfComments = commentStore.getComments.length
 
     // 3) Add a fake comment & test it was added successfully added
     let myComment = reactive({ text: 'Branch feature/delete-comment-preserve-replies' })
     await commentStore.addComment('entries', myComment, firstEntry.value)
 
-    // 4) Test to see that the number of comments has increased by one
-    await commentStore.fetchComments('entries', firstEntry.value.id) // ADD LISTENER
+    // Example usage
+    await waitUntil(() => {
+      return commentStore.getComments.length === startingNumberOfComments + 1
+    })
 
     expect(commentStore.getComments.length).toBe(startingNumberOfComments + 1)
 
     // 5): Delete fake comment. Start by retrieving it.
     let comments = commentStore.getComments
     let commentsOrdered = comments.sort((a, b) => b.created - a.created)
-    commentStore.deleteComment('entries', firstEntry.value.id, commentsOrdered[0].id)
-    await letSnapshotListenerRun(500)
-    await commentStore.fetchComments('entries', firstEntry.value.id)
+    await commentStore.deleteComment('entries', firstEntry.value.id, commentsOrdered[0].id)
+    // Example usage
     comments = commentStore.getComments
     commentsOrdered = comments.sort((a, b) => b.created - a.created)
+
+    await waitUntil(() => {
+      return commentStore.getComments.sort((a, b) => b.created - a.created)[0].text === 'Comment Deleted'
+    })
 
     // 6) Check to see that the comments has reduced back to the original value.
-    expect(commentsOrdered[0].text).toBe('Comment Deleted')
+    expect(commentStore.getComments.sort((a, b) => b.created - a.created)[0].text).toBe('Comment Deleted')
 
     // 7) remove the comment from the Firebase Store
-    commentStore.removeCommentFromFirestore('entries', firstEntry.value.id, commentsOrdered[0].id)
-    await letSnapshotListenerRun(500)
-    await commentStore.fetchComments('entries', firstEntry.value.id)
-    comments = commentStore.getComments
-    commentsOrdered = comments.sort((a, b) => b.created - a.created)
-    expect(comments.length).toBe(startingNumberOfComments)
+    await commentStore.removeCommentFromFirestore('entries', firstEntry.value.id, commentsOrdered[0].id)
+
+    await waitUntil(() => {
+      return commentStore.getComments.length === startingNumberOfComments
+    })
+
+    expect(commentStore.getComments.length).toBe(startingNumberOfComments)
   })
 })
 
