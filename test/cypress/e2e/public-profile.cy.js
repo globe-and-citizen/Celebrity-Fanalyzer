ff096addb445b30579ff00d5e8807fd1b0d7beb1
Arnon <arnonrdp@gmail.com>
2023-06-17 19:11:21 -0300

    test: added timeout

--- test/cypress/e2e/public-profile.cy.js
+++ test/cypress/e2e/public-profile.cy.js
@@ -2,13 +2,13 @@
 
 describe('Accessing a Public Profile', () => {
   it('Should navigate to a public profile page', () => {
-    // Visits the month page and waits for 5 seconds for the page to load
+    // Visits the month page
     cy.visit('/month')
 
     // Selects the author section on the page and clicks it
     cy.get('[data-test="author-section"]').click()
 
-    // Selects the posts card on the page and check if it has at least 1 post
-    cy.get('[data-test="posts-section"]').should('have.length.at.least', 1)
+    // Waits for the posts card on the page to load and checks if it has at least 1 post
+    cy.get('[data-test="posts-section"]', { timeout: 10000 }).should('have.length.at.least', 1)
   })
 })
