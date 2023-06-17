105ea3b3cb81ae9f5bf3ec1528627b2d2d74fccd
Arnon <arnonrdp@gmail.com>
2023-06-17 18:31:18 -0300

    Removed `fetchMonthPrompt` in favor of `fetchPrompts`

--- src/stores/prompts.js
+++ src/stores/prompts.js
@@ -1,77 +1,24 @@
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  limit,
-  onSnapshot,
-  orderBy,
-  query,
-  runTransaction,
-  setDoc,
-  Timestamp,
-  where
-} from 'firebase/firestore'
-import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { collection, deleteDoc, doc, onSnapshot, runTransaction, setDoc, Timestamp } from 'firebase/firestore'
+import { deleteObject, ref } from 'firebase/storage'
 import { defineStore } from 'pinia'
 import { db, storage } from 'src/firebase'
 import { useCommentStore, useEntryStore, useErrorStore, useLikeStore, useNotificationStore, useShareStore, useUserStore } from 'src/stores'
-import { currentYearMonth } from 'src/utils/date'
 
 export const usePromptStore = defineStore('prompts', {
   state: () => ({
     _isLoading: false,
-    _monthPrompt: null,
     _prompts: []
   }),
 
   persist: true,
 
   getters: {
-    getMonthPrompt: (state) => state._monthPrompt,
     getPromptRef: () => (id) => doc(db, 'prompts', id),
     getPrompts: (state) => state._prompts,
     isLoading: (state) => state._isLoading
   },
 
   actions: {
-    /**
-     * Fetch the current month prompt and set the value in the store :
-     * Checking if we have a data in the store.
-     * Check if we have data in the local storage.
-     * Fetch the data form firebase if 1-2 is false.
-     * @returns {Promise<void>}
-     */
-    async fetchMonthPrompt() {
-      const currentMonthId = currentYearMonth()
-
-      this._isLoading = true
-      const docSnap = await getDoc(doc(db, 'prompts', currentMonthId))
-
-      let prompt = {}
-
-      if (docSnap.exists()) {
-        prompt = docSnap.data()
-      } else {
-        await getDocs(query(collection(db, 'prompts'), orderBy('date', 'desc'), limit(1))).then(async (querySnapshot) => {
-          prompt = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))[0]
-        })
-      }
-
-      prompt.author = await getDoc(prompt.author).then((doc) => doc.data())
-      this._monthPrompt = prompt
-
-      if (prompt.entries?.length) {
-        for (const index in prompt.entries) {
-          prompt.entries[index] = await getDoc(prompt.entries[index]).then((doc) => doc.data())
-          prompt.entries[index].author = await getDoc(prompt.entries[index].author).then((doc) => doc.data())
-        }
-      }
-
-      this._isLoading = false
-    },
-
     async fetchPrompts() {
       const userStore = useUserStore()
 
