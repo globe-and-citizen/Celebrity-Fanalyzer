0b4e5c80b2e85dff298f03f7c55e0dacd9b30419
Arnon <arnonrdp@gmail.com>
2023-06-21 19:33:09 -0300

    Fixed set tab

--- src/pages/EntryPage.vue
+++ src/pages/EntryPage.vue
@@ -1,8 +1,8 @@
 <template>
   <q-tabs active-color="primary" class="bg-white fixed-bottom tab-selector" dense indicator-color="transparent" v-model="tab">
-    <q-tab content-class="q-ml-auto q-pb-sm" icon="fiber_manual_record" name="post" :ripple="false" />
-    <q-tab content-class="q-pb-sm" icon="fiber_manual_record" name="stats" :ripple="false" />
-    <q-tab content-class="q-mr-auto q-pb-sm" icon="fiber_manual_record" name="comments" :ripple="false" />
+    <q-tab content-class="q-ml-auto q-pb-md" icon="fiber_manual_record" name="post" :ripple="false" />
+    <q-tab content-class="q-pb-md" icon="fiber_manual_record" name="stats" :ripple="false" />
+    <q-tab content-class="q-mr-auto q-pb-md" icon="fiber_manual_record" name="comments" :ripple="false" />
   </q-tabs>
   <q-spinner v-if="!entry && entryStore.isLoading" class="absolute-center" color="primary" size="3em" />
   <q-tab-panels v-else animated class="bg-transparent col-grow" swipeable v-model="tab">
@@ -25,7 +25,6 @@
 import TheAnthrogram from 'src/components/Posts/TheAnthrogram.vue'
 import TheComments from 'src/components/Posts/TheComments.vue'
 import ThePost from 'src/components/Posts/ThePost.vue'
-import TheHeader from 'src/components/shared/TheHeader.vue'
 import { useCommentStore, useEntryStore, useErrorStore, useLikeStore, useShareStore } from 'src/stores'
 import { onMounted, onUnmounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -39,7 +38,7 @@ const likeStore = useLikeStore()
 const shareStore = useShareStore()
 
 const entry = ref({})
-const tab = ref('post')
+const tab = ref(entryStore.tab)
 
 onMounted(async () => {
   if (router.currentRoute.value.params.id) {
