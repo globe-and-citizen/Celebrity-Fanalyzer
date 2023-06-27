b823b72fdf5e41a152267b4a6f3bb425a876b0a5
Arnon <arnonrdp@gmail.com>
2023-06-27 16:44:27 -0300

    Improved request in SettingsTab

--- src/components/Profile/SettingsTab.vue
+++ src/components/Profile/SettingsTab.vue
@@ -1,22 +1,15 @@
 <template>
-  <h3 class="text-bold text-h5 text-secondary">Become a Writer!</h3>
-  <p class="text-body1 text-center">
-    We are always looking for new writers to join our team.
-    <br />
-    If you are interested, please send a message to us.
-  </p>
-  <q-input type="textarea" label="Message" v-model="message" />
-  <q-btn
-    class="full-width q-mt-lg"
-    color="primary"
-    data-test="become-writer"
-    label="Submit Request"
-    padding="12px"
-    rounded
-    @click="onBecomeWriter"
-  />
-
-  <q-separator spaced="xl" />
+  <q-form v-if="!userStore.isWriterOrAbove" @submit="onBecomeWriter">
+    <h3 class="text-bold text-h5 text-secondary">Become a Writer!</h3>
+    <p class="text-body1 text-center">
+      We are always looking for new writers to join our team.
+      <br />
+      If you are interested, please send a message to us.
+    </p>
+    <q-input class="q-mb-lg" label="Message" required type="textarea" v-model="message" />
+    <q-btn class="full-width" color="primary" data-test="become-writer" label="Submit Request" padding="12px" rounded type="submit" />
+    <q-separator spaced="xl" />
+  </q-form>
 
   <q-input v-model="userStore.getUser.email" disable label="Email" />
   <q-btn class="full-width q-mt-lg" color="secondary" data-test="logout-button" label="Logout" padding="12px" rounded @click="onLogout" />
@@ -34,11 +27,6 @@ const requestStore = useRequestStore()
 const userStore = useUserStore()
 
 const message = ref('')
-// const user = ref(userStore.getUser)
-
-// userStore.$subscribe((_mutation, state) => {
-//   user.value = state._user
-// })
 
 function onBecomeWriter() {
   requestStore
