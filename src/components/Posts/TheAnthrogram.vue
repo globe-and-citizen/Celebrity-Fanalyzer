5cbbd265d49c897f17336ed90e1cbf447949b739
Arnon <arnonrdp@gmail.com>
2023-06-30 19:40:12 -0300

    Removed unused code

--- src/components/Posts/TheAnthrogram.vue
+++ src/components/Posts/TheAnthrogram.vue
@@ -27,14 +27,11 @@
 </template>
 
 <script setup>
-import { Timestamp } from 'firebase/firestore'
-import VisitorsBar from 'src/components/Graphs/VisitorsBar.vue'
 import LikesBar from 'src/components/Graphs/LikesBar.vue'
 import SharesPie from 'src/components/Graphs/SharesPie.vue'
+import VisitorsBar from 'src/components/Graphs/VisitorsBar.vue'
 import TheHeader from 'src/components/shared/TheHeader.vue'
 import { useLikeStore, useShareStore, useVisitorStore } from 'src/stores'
-import { getStats } from 'src/utils/date'
-import { formatAllStats, formatStats } from 'src/utils/stats'
 import { ref } from 'vue'
 
 const props = defineProps(['post'])
@@ -46,39 +43,7 @@ const visitorStore = useVisitorStore()
 const shares = ref(shareStore.getShares)
 const type = ref('daily')
 
-const updateChartData = () => {
-  if (!props.post.created) return
-  const { weekStats, dayStats } = getStats(likeStore.$state, props.post.created)
-  const allStats = [
-    {
-      date: Timestamp.fromDate(new Date()),
-      likes: likeStore.getLikes.length,
-      dislikes: likeStore.getDislikes.length
-    }
-  ]
-  return { weekStats, dayStats, allStats }
-}
-const chartData = ref(updateChartData())
-
-likeStore.$subscribe(() => {
-  chartData.value = updateChartData()
-})
-
 shareStore.$subscribe((_mutation, state) => {
   shares.value = state._shares
 })
-
-function graphData(type) {
-  if (!chartData.value) return []
-
-  if (type === 'daily') {
-    return formatStats(chartData.value.dayStats, 'daily')
-  }
-
-  if (type === 'weekly') {
-    return formatStats(chartData.value.weekStats, 'weekly')
-  }
-
-  return formatAllStats(chartData.value.allStats)
-}
 </script>
--- src/utils/stats.js
+++ src/utils/stats.js
@@ -1,20 +1,3 @@
-export const formatStats = (stats, interval) => {
-  const formattedStats = stats?.map((stat, index) => {
-    const date = stat.date.toDate()
-    const end = stats[index + 1]?.date.toDate() || date
-    const label =
-      end - date <= 86400000 ? `${date.getDate()}/${date.getMonth() + 1}` : `${date.getDate()}-${end.getDate()}/${end.getMonth() + 1}`
-    return { ...stat, label }
-  })
-
-  formattedStats?.pop()
-  return formattedStats
-}
-
-export const formatAllStats = (stats) => {
-  return stats?.map((stat) => ({ ...stat, label: 'All' }))
-}
-
 /**
  * Group data by week.
  * @param {*} data = [
