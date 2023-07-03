a0f246943c09cd29c76ffeea2c857cdff4e9006d
Arnon <arnonrdp@gmail.com>
2023-07-03 18:16:16 -0300

    Created function to group shares by date

--- src/components/Posts/Graphs/SharesPie.vue
+++ src/components/Posts/Graphs/SharesPie.vue
@@ -7,16 +7,15 @@ import { PieChart } from 'echarts/charts'
 import { LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components'
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
+import { monthDayYear } from 'src/utils/date'
 import { ref, watchEffect } from 'vue'
 import VChart from 'vue-echarts'
 
 use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent])
 
-const props = defineProps({
-  data: { type: Array, required: true },
-  interval: { type: String, default: 'daily' }
-})
+const props = defineProps(['data', 'interval'])
 
+const dates = ref([])
 const option = ref({})
 const platforms = [
   { name: 'Clipboard', color: '#777777' },
@@ -36,56 +35,118 @@ const sharesCount = ref(0)
 function compute() {
   option.value = {
     title: {
-      text: `${sharesCount.value} Shares`,
+      text: `${props.data.length} Shares`,
       left: 'center'
     },
     tooltip: {
-      trigger: 'item',
-      formatter: '{b}: {c} ({d}%)'
+      trigger: 'axis'
     },
     legend: {
-      orient: 'vertical',
-      left: 'left'
+      bottom: 0
+    },
+    xAxis: {
+      type: 'category',
+      data: ['30 Jun', '1 Jul', '2 Jul', '3 Jul'] // dates.value
+    },
+    yAxis: {
+      type: 'value'
     },
     series: [
       {
-        type: 'pie',
-        radius: '55%',
-        data: shares,
-        colorBy: 'data',
-        color: shares.value.map((share) => share.color)
+        name: 'Clipboard',
+        type: 'bar',
+        stack: 'Total',
+        data: [120, 132, 101, 134],
+        color: '#777777'
+      },
+      {
+        name: 'Facebook',
+        type: 'bar',
+        stack: 'Total',
+        data: [220, 182, 191, 234],
+        color: '#4267B2'
       }
     ]
   }
 }
 
-const intervalFunctions = {
-  daily: (shareDate, now) => shareDate >= new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-  weekly: (shareDate, now) => {
-    const weekStart = new Date(now.setDate(now.getDate() - now.getDay()))
-    return shareDate >= weekStart
-  },
-  monthly: () => true
+function groupSharesByDate(data) {
+  // create a new object that will hold the results
+  const sharesByDate = data.reduce((acc, item) => {
+    const date = monthDayYear(item.createdAt)
+    if (acc[date]) {
+      // add the share to the date
+      acc[date][item.sharedOn] = (acc[date][item.sharedOn] || 0) + 1
+    } else {
+      // otherwise, set the date and add the share
+      acc[date] = { [item.sharedOn]: 1 }
+    }
+    return acc
+  }, {})
+
+  // convert the object into a sorted array
+  const result = Object.keys(sharesByDate).map((date) => ({ [date]: sharesByDate[date] }))
+
+  // sort the array by date
+  result.sort((a, b) => {
+    var dateA = Object.keys(a)[0]
+    var dateB = Object.keys(b)[0]
+    return new Date(dateA) - new Date(dateB)
+  })
+
+  const uniqueDates = []
+  const today = new Date()
+  const firstDate = Object.keys(result[0])[0]
+
+  let currentDate = new Date(firstDate)
+
+  // Generate missing dates between the first provided date and today's date
+  while (currentDate <= today) {
+    const formattedDate = currentDate.toLocaleDateString('en-US')
+    if (!uniqueDates.includes(formattedDate)) {
+      uniqueDates.push(formattedDate)
+    }
+    currentDate.setDate(currentDate.getDate() + 1)
+  }
+
+  // Add missing dates to the result
+  uniqueDates.forEach((date) => {
+    if (!result.find((item) => Object.keys(item)[0] === date)) {
+      result.push({ [date]: {} })
+    }
+  })
+
+  // sort the array by date
+  result.sort((a, b) => {
+    var dateA = Object.keys(a)[0]
+    var dateB = Object.keys(b)[0]
+    return new Date(dateA) - new Date(dateB)
+  })
+
+  return result
 }
 
 watchEffect(() => {
-  shares.value = platforms
-    .map((platform) => {
-      const count = props.data.reduce((acc, share) => {
-        const shareDate = new Date(share.createdAt.seconds * 1000)
-        const now = new Date()
+  if (!props.data.length) return
 
-        if (platform.name.toLowerCase() === share.sharedOn.toLowerCase() && intervalFunctions[props.interval](shareDate, now)) {
-          return acc + 1
-        }
-        return acc
-      }, 0)
+  const info = groupSharesByDate(props.data)
+  console.log('info', JSON.stringify(info, null, 2))
 
-      return { name: platform.name, value: count, color: platform.color }
-    })
-    .filter((platform) => platform.value > 0)
+  // shares.value = platforms
+  //   .map((platform) => {
+  //     const count = props.data.reduce((acc, share) => {
+  //       const shareDate = new Date(share.createdAt.seconds * 1000)
+  //       const now = new Date()
 
-  sharesCount.value = shares.value.reduce((acc, share) => acc + share.value, 0)
+  //       if (platform.name.toLowerCase() === share.sharedOn.toLowerCase() && intervalFunctions[props.interval](shareDate, now)) {
+  //         return acc + 1
+  //       }
+  //       return acc
+  //     }, 0)
+
+  //     return { name: platform.name, value: count, color: platform.color }
+  //   })
+  //   .filter((platform) => platform.value > 0)
 
   compute()
 })
