fbca67c333e866039783ccba6247afe4661ee55c
Hermann EHO <hermannleboss@gmail.com>
2023-08-01 10:25:45 +0000

    test: fix login test

--- test/cypress/e2e/login-logout.cy.js
+++ test/cypress/e2e/login-logout.cy.js
@@ -3,25 +3,19 @@
 // See https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
 
 describe('Login page', () => {
-  context('login then', () => {
-    beforeEach(() => {
-      cy.viewport('iphone-x')
-      cy.visit('/profile')
+  it('Login Then Logout', () => {
+    cy.viewport('macbook-16')
+    cy.visit('/profile')
 
-      // Fill the email and password fields and click the sign in button
-      cy.get('[data-test="email-field"]').type('test@test.com')
-      cy.get('[data-test="password-field"]').type('12345678')
-      cy.get('[data-test="sign-button"]').click()
-    })
+    // Fill the email and password fields and click the sign in button
+    cy.get('[data-test="email-field"]').type('test@test.com')
+    cy.get('[data-test="password-field"]').type('12345678')
+    cy.get('[data-test="sign-button"]').click()
 
-    it('Should navigate between Profiles tabs', () => {
-      cy.getByData('tab-settings').click({ force: true })
-      cy.getByData('tab-feedback').click()
-      cy.getByData('tab-profile').click()
-    })
-    it('Logout', () => {
-      cy.getByData('tab-settings').click({ force: true })
-      cy.getByData('logout-button').click()
-    })
+    cy.getByData('tab-settings').click({ force: true })
+    cy.getByData('tab-feedback').click()
+    cy.getByData('tab-profile').click()
+    cy.getByData('tab-settings').click({ force: true })
+    cy.getByData('logout-button').click()
   })
 })
