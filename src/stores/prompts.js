1ab80e7b4e54475327cb81a60fb39d157c23a721
Hermann EHO <hermannleboss@gmail.com>
2023-08-08 18:45:01 +0000

    fix: fix prompt author

--- src/stores/prompts.js
+++ src/stores/prompts.js
@@ -29,18 +29,18 @@ export const usePromptStore = defineStore('prompts', {
       }
 
       this._isLoading = true
-      onSnapshot(collection(db, 'prompts'), (querySnapshot) => {
-        const prompts = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      onSnapshot(collection(db, 'prompts'), async (querySnapshot) => {
+        const prompts = querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
 
         for (const prompt of prompts) {
-          prompt.author = userStore.getUserById(prompt.author.id) || prompt.author
+          prompt.author = userStore.getUserById(prompt.author.id) || await userStore.fetchUser(prompt.author.id)
           prompt.entries = prompt.entries?.map((entry) => entry.id)
         }
 
         prompts.reverse()
 
         this._prompts = []
-        this.$patch({ _prompts: prompts })
+        this.$patch({_prompts: prompts})
       })
       this._isLoading = false
     },
