8db66d8ae55e0299c159c435a6e27d440bfaec77
Arnon <arnonrdp@gmail.com>
2023-08-11 06:13:51 -0300

    Wait before go to Prompt Page

--- test/cypress/e2e/search.cy.js
+++ test/cypress/e2e/search.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 /// <reference types="cypress" />
 
 describe('Search page', () => {
@@ -7,6 +8,7 @@ describe('Search page', () => {
 
     cy.get('[data-test="search-input"]').type(' ')
     cy.get('[data-test="prompt-card"] > :first-child').click()
+    cy.wait(1000)
     cy.get('[data-test="title"]').should('have.text', 'Prompt Page')
   })
 })
