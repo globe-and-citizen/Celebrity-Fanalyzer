80ef9fece938b81b9004aa5002ad0f9ab8aba2c4
Arnon R <arnonrdp@gmail.com>
2023-10-24 11:51:09 -0300

    Removed unnecessary `_isLoading`

--- src/stores/stats.js
+++ src/stores/stats.js
@@ -5,18 +5,15 @@ import { useUserStore } from 'src/stores'
 
 export const useStatStore = defineStore('stats', {
   state: () => ({
-    _isLoading: false,
     _stats: undefined
   }),
 
   getters: {
-    isLoading: (state) => state._isLoading,
     getStats: (state) => state._stats
   },
 
   actions: {
     async fetchStats(collectionName, documentId) {
-      this._isLoading = true
       onSnapshot(collection(db, collectionName, documentId, 'stats'), (querySnapshot) => {
         this._stats = querySnapshot.docs.map((doc) => doc.data())
       })
@@ -29,9 +26,7 @@ export const useStatStore = defineStore('stats', {
       stats.author = userStore.isAuthenticated ? userStore.getUserRef : userStore.getUserIpHash
       stats.created = Timestamp.fromDate(new Date())
 
-      this._isLoading = true
       await addDoc(collection(db, collectionName, documentId, 'stats'), stats)
-      this._isLoading = false
     }
   }
 })
