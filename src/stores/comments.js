c9323d6daadc11448c52314b27eda63532e18270
Javokhir Nematov <nematovjavohir58@gmail.com>
2023-12-09 14:13:06 +0500

    Removed the console.logs

--- src/stores/comments.js
+++ src/stores/comments.js
@@ -168,9 +168,6 @@ export const useCommentStore = defineStore('comments', {
     },
 
     async deleteComment(collectionName, documentId, commentId) {
-      console.log('Collection name: ', collectionName)
-      console.log('Document ID: ', documentId)
-      console.log('Comment ID: ', commentId)
       const userStore = useUserStore()
       await userStore.fetchUserIp()
 
