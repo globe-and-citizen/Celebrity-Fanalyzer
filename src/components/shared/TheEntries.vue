f33e3205de46c37807e02d7deadb96be351f4cac
Arnon R <arnonrdp@gmail.com>
2024-07-27 11:06:56 -0300

    feat: update entry button creation UI in TheEntries component

--- src/components/shared/TheEntries.vue
+++ src/components/shared/TheEntries.vue
@@ -4,12 +4,12 @@
     :data-test="props.entries ? 'entries' : ''"
     style="padding-bottom: 7rem"
   >
-    <h2 class="q-my-auto text-bold text-h5 q-pa-md">
-      Entries
-      <q-icon v-if="!hasWinner" class="cursor-pointer" name="add" size="30px" @click="openEntryDialog()">
-        <q-tooltip>Add entries</q-tooltip>
-      </q-icon>
-    </h2>
+    <div class="flex items-center">
+      <h2 class="q-my-auto text-bold text-h5 q-pa-md">Entries</h2>
+      <q-btn v-if="!hasWinner" color="primary" dense icon="add" outline round @click="openEntryDialog">
+        <q-tooltip>Create entry</q-tooltip>
+      </q-btn>
+    </div>
     <div class="card-items-wrapper">
       <ItemCard v-for="entry in entries" :item="entry" :key="entry?.id" :link="entry.slug" data-test="entry" />
     </div>
