788e90f025006a88c25302963ac95291f25c77d8
Lupanciuc Marc <marclupanc@gmail.com>
2024-11-05 23:43:37 +0200

    fix:#521 - lighthouse pipeline fix

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,8 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './dist',
-      url: ['http://localhost:9200']
+      startServerCommand: 'serve -s build -l 9200',
+      url: ['http://localhost:9200/'],
+      numberOfRuns: 3
+    },
+    upload: {
+      target: 'temporary-public-storage'
     },
     assert: {
       assertions: {
