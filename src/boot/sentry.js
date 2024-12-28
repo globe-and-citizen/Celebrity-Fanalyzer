f1961d6da9e3c198a251ed88a5e511c2e5c590ef
Lupanciuc Marc <marclupanc@gmail.com>
2024-12-28 17:34:05 +0200

    feat:#674 - remove env variable, ignore env, update trace targets

--- src/boot/sentry.js
+++ src/boot/sentry.js
@@ -3,12 +3,12 @@ import * as Sentry from '@sentry/vue'
 export default ({ app }) => {
   Sentry.init({
     app,
-    dsn: 'https://fbf6f17dc1de5d78955f7e69a6a16d76@o4507662781382656.ingest.us.sentry.io/4507662784069632',
+    dsn: import.meta.env.VITE_SENTRY_DSN,
     integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
     // Performance Monitoring
     tracesSampleRate: 1.0,
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ['localhost', /^https:\/\/feat-sentry\.celebrity-fanalyzer\.pages\.dev/],
+    tracePropagationTargets: [/^https:\/\/celebrityfanalyzer\.com/],
     // Session Replay
     // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysSessionSampleRate: 0.1,
