e02cf035c90c07492a0ba3f33a5f91dfb266626a
Sana Yasmin <sanayasmin94@gmail.com>
2025-01-01 22:39:19 +0530

    feat:#684 - Delete account

--- src/components/Profile/SettingsTab.vue
+++ src/components/Profile/SettingsTab.vue
@@ -1,15 +1,44 @@
 <template>
   <q-input v-model="userStore.getUser.email" disable label="Email" />
   <q-btn class="full-width q-mt-lg" color="secondary" data-test="logout-button" label="Logout" padding="12px" rounded @click="onLogout" />
+
+  <q-btn
+    class="full-width q-mt-lg"
+    color="negative"
+    data-test="delete-account-button"
+    label="Delete Account"
+    padding="12px"
+    rounded
+    @click="openDeleteConfirmationDialog"
+  />
+
+  <q-dialog v-model="deleteDialog">
+    <q-card>
+      <q-card-section class="q-pb-none">
+        <h6 class="q-my-sm">Delete Account?</h6>
+      </q-card-section>
+      <q-card-section>
+        Hi
+        <strong>{{ userStore.getUser.displayName }}</strong>
+        , are you sure you want to delete your account? This action is irreversible.
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="Cancel" color="primary" v-close-popup />
+        <q-btn data-test="delete-button" flat label="Delete" color="negative" @click="onDeleteAccount" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup>
 import { useUserStore } from 'app/src/stores'
 import { useRouter } from 'vue-router'
 import { customWeb3modal } from 'src/web3/walletConnect'
+import { ref } from 'vue'
 
 const userStore = useUserStore()
 const router = useRouter()
+const deleteDialog = ref(false)
 
 function onLogout() {
   if (customWeb3modal.getAddress()) {
@@ -17,5 +46,20 @@ function onLogout() {
   }
   userStore.logout()
   router.push({ path: '/profile' })
+}
+
+function openDeleteConfirmationDialog() {
+  deleteDialog.value = true
+}
+
+async function onDeleteAccount() {
+  try {
+    await userStore.deleteOwnAccount()
+    router.push({ path: '/profile' })
+  } catch (error) {
+    console.error('Account deletion failed:', error)
+  } finally {
+    deleteDialog.value = false
+  }
 }
 </script>
